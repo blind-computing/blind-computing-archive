cdc3004f80d14aaf25563cddef368df72747d523
techified-mcb <mikeybuchan@hotmail.co.uk>
2016-10-22 22:39:09 +0100

    made github link in index.php file oautomatically open in a new tab

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo '<div id="content">
 <p>Blind computing is made to be the hub of content for blind and visually impared users that need or want to use a computer. We will try to cover as much as we can, from blind/vi devices, to the latest computer operating systems, all the way down to accessible tools, programs and web sites that contributers have found useful.</p>
 <h2>how to contribute</h2>
 <p>Please e-mail me by <a href="mailto:contribute@blind-computing.comlu.com" title="blind-computing contribute e-mail">clicking here</a> or by e-mailing contribute@blind-computing.comlu.com</p>
-<p>You can also submit a pull request to the github repository found <a href="https://github.com/mcb2003/blind-computing">here</a>. You may need to prod me a bit by e-mail so I actually re-upload the files, though, I might not remember!</p>
+<p>You can also submit a pull request to the github repository found <a href="https://github.com/mcb2003/blind-computing" target="_blank" title="opens in a new tab">here</a>. You may need to prod me a bit by e-mail so I actually re-upload the files, though, I might not remember!</p>
 </div>
 </body></html>';
 ?>
