751a2f4681a256bd2a674addf51405c495ba06cf
techified-mcb <mikeybuchan@hotmail.co.uk>
2016-10-22 22:10:58 +0100

    added previous web site files

---
+++ includes/nav.inc.php
@@ -0,0 +1,21 @@
+<?php
+echo '<!doctype html>
+<html>
+<head>
+<title>blind computing - documenting the termoil between blind computer users and computers</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head><body lang="en-GB">
+<div id="headerbar">
+<strong style="float:left;"><a href="index.html" title="go to the home page">/</a></strong>
+<center style="float:right;" class="main-header"><strong>blind-computing | home</strong></center>
+</div>
+<aside id="sidebar">
+<strong style="float:left;">navigation</strong>
+<a style="float:right;" href="#content" title="skips to the main content on the page">skip to content</a>
+<ul id="navigation">
+<li class="nav-item"><a href="index.php">home</a></li>
+<li class="nav-item"><a href="notetakers.php">braille notetakers and devices</a></li>
+<li class="nav-item"><a href="operating-systems.php">operating system accessibility</a></li>
+</ul>
+</aside>';
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include("includes/nav.inc.php");
+echo '<div id="content">
+<h2>What is blind computing</h2>
+<p>  In early September 2016, 13 year old Michael C Buchan was getting quite frustrated. There simply wasn't any up to date, useful content on the internet about blind devices, software and other accessible computer related things. So, he created blind computing.</p>
+<p>Blind computing is made to be the hub of content for blind and visually impared users that need or want to use a computer. We\'ll try to cover as much as we can, from blind/vi devices, to the latest computer operating systems, all the way down to accessible tools, programs and web sites that contributers have found useful.</p>
+<h2>how to contribute</h2>
+<p>Please e-mail me by <a href="mailto:contribute@blind-computing.comlu.com" title="blind-computing contribute e-mail">clicking here</a> or by e-mailing contribute@blind-computing.comlu.com</p>
+</div>
+</body></html>';
+?>
