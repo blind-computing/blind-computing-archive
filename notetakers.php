ba30ad2412b6843141465d339806d7635a166448
techified-mcb <mikeybuchan@hotmail.co.uk>
2016-10-23 09:38:54 +0100

    changed wording of heading 3 to say list of resources

--- notetakers.php
+++ notetakers.php
@@ -4,7 +4,7 @@ echo '
 <div id="content">
 <h2>braille note takers and devices</h2>
 <p>This page focusses on the category of braille notetakers. From fully featured PDA devices with braille displays, right down to simple text editting machines. We\'ll even cover devices with out braille displays, such as humanware\'s voicenote.</p>
-<h3>list of articles</h3>
+<h3>list of resources</h3>
 <ul>
 <li>Michael Buchan wrote <a href="braillenote-esytime-report.php">a comparison of the braillenote touch and the esytime braille computers</a>.</li>
 </ul>
