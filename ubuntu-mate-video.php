fd015825910d1f719a74c59b159b256b9505dff6
techified-mcb <mikeybuchan@hotmail.co.uk>
2016-10-23 14:57:34 +0100

    added heading for video info and mostly fixed sizing issues on smaller devices

--- ubuntu-mate-video.php
+++ ubuntu-mate-video.php
@@ -4,10 +4,11 @@ echo '<div id="content">
 <h2>ubuntu mate 16.04 accessibility</h2>
 <p>  Ubuntu mate is quickly growing to be a well respected, clean, easy to use linux distrobution.It has the latest features, the coolest software and the best desktop interface (in my opinion), but does it have the accessibility features that blind/vi people need out of an operating system?</p>
 <p>  Actually, the answer looks very promissing</p>
-<p>here\'s a quick video showing the accessibility features of ubuntumate 16.04 lts.</p>
+<p>here\'s a quick video showing the accessibility features of ubuntu mate 16.04 lts.</p>
 
-<iframe style="max-width:40%;width:640px;height:480px;float:left;" src="https://www.youtube.com/embed/yCfBoCzOoEE" frameborder="0" allowfullscreen></iframe>
+<iframe style="max-width:60%;width:640px;height:480px;float:left;" src="https://www.youtube.com/embed/yCfBoCzOoEE" frameborder="0" allowfullscreen></iframe>
 <div id="video-info">
+<h3>youtube video description</h3>
 <p>Published on 12 Oct 2016<p>
 <p>A quite long, rambling, keybord thwacking video showing the features linux provides for blind and visually impared users. We cover compiz negative and zoom features, firefox settings to make it easier to see your web pages and the orca screen reader.</p>
 </div>
