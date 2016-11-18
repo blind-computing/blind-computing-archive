c64ba212af851f8a5a181112e192abd65894990e
techified-mcb <mikeybuchan@hotmail.co.uk>
2016-11-18 19:47:43 +0000

    changed naming of files

--- notetakers.php
+++ notetakers.php
@@ -6,7 +6,7 @@ echo '
 <p>This page focusses on the category of braille notetakers. From fully featured PDA devices with braille displays, right down to simple text editting machines. We\'ll even cover devices with out braille displays, such as humanware\'s voicenote.</p>
 <h3>list of resources</h3>
 <ul>
-<li>Michael Buchan wrote <a href="braillenote-esytime-report.php">a comparison of the braillenote touch and the esytime braille computers</a>.</li>
+<li>Michael Buchan wrote <a href="braillenote-esytime-1.php">a comparison of the braillenote touch and the esytime braille computers</a>.</li>
 </ul>
 </div>
 </body></html>
