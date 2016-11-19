a269a6e3e01b5b329c6c6067b429fff161b230a5
mcb2003 <mikeybuchan@hotmail.co.uk>
2016-11-19 14:56:48 +0000

    modified:   braillenote-esytime-2.php
    massively cleaned up the html code inside the file.x

--- braillenote-esytime-2.php
+++ braillenote-esytime-2.php
@@ -9,11 +9,8 @@ echo '
 <link rel="stylesheet" type="text/css" href="css/report.css">
 <center><h1>Braillenote Touch VS Esytime - After Hands On</h1></center>
 <h2>Introduction</h2>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
-<br/>
 
-</p>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+<p>
   Recently, I started looking to get my old, tatty braillenote apex
 bt32 notetaker replaced with something new. I, along with my VI team
 at school found 2 devices. The braillenote touch, which is a braille
@@ -26,120 +23,111 @@ until that point. After that, I got somebody from each side of the
 battle to come out and demonstrate there product. This, my second
 report, is the most important notes <span style="font-style: normal">I
 found out from these people and further research.</span></p>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><br/>
 
-</p>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><u> 
+<p><u> 
 <b>I’ll also publish a list of questions I had for the
 aformentioned sails people later, so you can see what my thought
 process was.</b></u></p>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><br/>
 
-</p>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><u> 
+<p><u> 
 <b>Also note that this document was written for me and the VI team to
 decide which device to buy, if any and was not originally meant to be
 published here.</b></u></p>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><br/>
 
-</p>
 <h2>Contents</h2>
+ 
 <ol>
 <li><a href="#similarities">similarities between both devices</a></li>
 <li><a href="#bntnotes">notes on the braillenote touch</a></li>
 <li><a href="#esynotes">Notes on the esytime</a></li>
 <li><a href="#summery">summery</a></li>
-</ol>
+</ol> 
 
 <h2 id="similarities">Similarities between both units</h2>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><br/>
 
-</p>
-<ol>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+<ol> 
+	<li>
+<p>
 	Both devices seem to be bigger than my current braillenote apex. The
 	braillenote seems to have a larger surface area, but the esytime
 	seems to be thicker.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Both units have standard cursor keys above the braille display.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Both units have a 32 cell braille display. The braillenote touch has
 	an option for an 18 cell display, but I’m not sure about the
 	esytime.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Both products have clear, sharp braille displays, at least to my
 	fingers.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	They both are considerably more advanced and overall more useful
 	than my braillenote apex.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Both take longer to boot up from a cold start than my braillenote,
 	but unless the devices crash, this shouldn’t be an issue, because
 	I can leave the chosen device in sleep mode.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Both devices have a braille terminal, allowing the user to connect
 	the display to another computer and enter/read text.</p>
+ </li>
 </ol>
-
 <h2 id="bntnotes">Notes on the braillenote touch</h2>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><br/>
 
-</p>
-<ol>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+ <ol>
+	<li>
+<p>
 	Runs android 4.4 kitkat, which is several versions out of date. It
 	still does have security updates directly from google, however.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	The touch braille works remarkably better than I’m sure anybody
 	expected and I can easily get used to it. The included keyboard is
 	also very simple to use and it is not much different from the
 	braillenote apex.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Key math, which is the program used to enter math equasions, could
 	come in very handy and once we work out the technical side of
 	things, I could much more easily do class and home work using it.
 	<b>NOTE: The esytime does integrate  similar functionality, but I
 	haven’t tried it.</b></p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	KNFB reader seems to work well enough (I gave it a bulletted list
 	and it put all the numbers before the text, but that’s managable).</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Because of the fact that the touch comes with android, the google
 	integration (which will be useful for me in school) is very tight,
 	as google makes android</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Because the braillenote touch has a screen, classwork will be easier
 	because the teacher can read what’s being entered at the press of
 	a key (when editting text, only the braille dots show up on the
 	display and you have to hit the visual preview button to see it as
 	rendered text). The esytime requires an external display.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	The braillenote touch is much more customised than the esytime. In
 	some ways, this is better and in others, it’s worse. For example,
 	the touch has it’s own menu for selecting which voice to use,
 	whereas the esytime relies on the screen reader’s settings dialog.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	The braillenote has a built-in camera on the botton of the device,
 	whereas the esytime does not have any camera. The esytime does have
 	more usb ports though (3 compared to 1), so the required facilities
 	are there to connect an external camera.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	One thing that bugs me is that the touch has changed the navigation
 	keys from space and backspace, to space with dot one and space with
 	dot 4 respectively. Thumb key navigation is still possible, but this
@@ -147,19 +135,17 @@ published here.</b></u></p>
 	opinion, a better navigation controller (2 joy sticks that can, with
 	the right screen reader, be customised to act as many different
 	things, but by default, both act as the arrow keys).</p>
+ </li>
 </ol>
-
 <h2 id="esynotes">Notes on the esytime</h2>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><br/>
 
-</p>
-<ol>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+ <ol>
+	<li>
+<p>
 	The device is made of metal and although this will make it heavier,
 	I think it’s better in the long run more durability.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	The design seems a little unconventional, with the top portion of
 	the device being the braille display and the bottom being the
 	braille keyboard. There are no thumb keys (panning is achieved
@@ -168,62 +154,59 @@ published here.</b></u></p>
 	the backspace key and the right hand one as a normal space. Both
 	keys together act as enter. This is required because of the way
 	screen readers interface with the keyboard.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	The keyboard it’s self feels quite nice and is very quiet, though
 	not as quiet as the touch braille.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	When using NVDA or window eyes, grade 2 can not be entered natively,
 	however, the esysuite package provides the ability to bypass this
 	limmitation and type in grade 2. Other screen readers support native
 	grade 2 entry. This wouldn’t really bug me that much, however.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	As mentioned in the first report, Any software program that can run
 	on windows (the operating system of choice for the school) will
 	work, even without an accessable framework in place. A mouse and
 	screen can be connected and the esytime can be used just like my
 	current laptop, even allowing for transcription to happen on the
 	device itself (although this is not very practical).</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Like the braillenote, the esytime can attempt to use OCR to read PDF
 	files, but there is no direct way to take a picture of something and
 	have it appear on the screen.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	The esytime seems to be quite good with different types of braille,
 	such as braille maths and braille music.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	The braille music functionality allows the creation of midi files, a
 	feature that I could make much use of at home, although purely for
 	fun.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Unlike the braillenote touch, the esytime would allow me to do all
 	of my computing work directly on it, as any programming language
 	that can work under windows will work. Android will definitely not
 	be able to do this.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	Even with moddern technology, I still am unsure as to whether a
 	spinning hard drive is a good idea in a school environment (I’m
 	sure it would work fine?) But this shouldn’t be a problem, as a
 	solid state version is due to be released.</p>
-	<li/>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+	</li><li>
+<p>
 	The esytime does not come with a case, nor is one available to my
 	knolage. This shouldn’t be much of a problem, but it’s a
 	consideration  non the less.</p>
+ </li>
 </ol>
-
 <h2 id="summery">Summery</h2>
-<p align="left" style="margin-bottom: 0cm; line-height: 100%"><br/>
-
-</p>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+<p>
   I’m still not sure which unit is better for me. Mainly because of
 the computing work I’ll have to do, I think the esytime is the
 better braille device. But then again, the braillenote is already
@@ -233,17 +216,11 @@ products. It’s just a matter of do you want a braille tablet or a
 braille computer. Most geeks, at least, would immediately say the
 latter, but then again, a computer has more parts and is more complex
 than a tablet.</p>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
-<br/>
 
-</p>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+<p>
 Thanks for reading this epic of a report.</p>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
-<br/>
 
-</p>
-<p align="left" style="margin-bottom: 0cm; font-weight: normal; line-height: 100%; text-decoration: none">
+<p>
 Mikey</p>
 </body>
 </html></div>
