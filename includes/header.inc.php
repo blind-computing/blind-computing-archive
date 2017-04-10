a61994a88795b8565dad092eb9b08f261d96cff2
mcb2003 <mikeybuchan@hotmail.co.uk>
2017-04-10 21:48:02 +0100

    updated all files

---
+++ includes/header.inc.php
@@ -0,0 +1,27 @@
+<?php
+echo '
+<!doctype html>
+<html>
+<head>
+<title>blind computing - documenting the termoil between blind computer users and computers</title>
+<	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="generator" content="LibreOffice 5.1.3.2 (Linux)"/>
+	<meta name="created" content="2016-09-12T22:28:02.595805800"/>
+	<meta name="changed" content="2016-09-15T22:32:46.566846800"/>
+	/head><body lang="en-GB" dir="ltr">
+<div id="headerbar">
+<strong style="float:left;"><a href="index.html" title="go to the home page">/</a></strong>
+<center style="float:right;" class="main-header"><strong>blind-computing | home</strong></center>
+</div>
+<aside id="sidebar">
+<strong style="float:left;">navigation</strong>
+<a style="float:right;" href="#content" title="skips to the main content on the page">skip to content</a>
+<ul id="navigation">
+<li class="nav-item"><a href="index.html">home</a></li>
+<li class="nav-item">esytime vs braillenote touch</li>
+</ul>
+</aside>
+<div id="content">
+';
+?>
