f2e6c7764cd1a5b22b61d46f4f62a49dfff0801c
mcb2003 <mikeybuchan@hotmail.co.uk>
2017-05-02 19:20:17 +0100

    added link to linux-accessibility.php in operating-systems.php

--- operating-systems.php
+++ operating-systems.php
@@ -7,6 +7,7 @@ echo '
 <h3>list of resources</h3>
 <ul>
 <li>  Michael Buchan made <a href="ubuntu-mate-video.php"> a video showing the main accessibility features of ubuntu mate 16.04</a>.</li>
+<li>Michael Buchan also wrote <a href="linux-accessibility.php">an article going into great detail about using linux with a screen reader, zoom and negative features</a>.</li>
 </ul>
 </div>
 </body></html>
