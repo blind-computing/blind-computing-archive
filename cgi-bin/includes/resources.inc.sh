703bd2bc4eac5458077e676de00239a636e82806
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 10:14:27 +0000

    Changes include: * Renamed devices.inc.sh to resources.inc.sh as this file will be used on multiple pages. * Modified resources.inc.sh so that it displays the correct file in the correct format. * Created a test devices-items.csv file to check if the code works (not testied yet)

---
+++ cgi-bin/includes/resources.inc.sh
@@ -0,0 +1,15 @@
+cat <<END-OF-CAT
+<ul class="resource-list">
+END-OF-CAT
+cat includes/${1}-items.csv | while read line
+do
+prefix="$(echo $line | cut -d, -f1 -)"
+lnktxt="$(echo $line | cut -d, -f2 -)"
+page="$(echo $line | cut -d, -f3 -)"
+printline="<li class=\"resource\">${prefix} <a href=\"${page}\">${lnktxt}</a></li>"
+echo "$printline"
+done
+cat <<END-OF-CAT
+</ul>
+</div>
+END-OF-CAT
