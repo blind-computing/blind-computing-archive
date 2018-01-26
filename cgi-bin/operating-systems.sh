99de1c0274a78ec5c82d48c18819a350f1cc1cd9
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 11:22:31 +0000

    added operating-systems.sh page with no resources yet.

---
+++ cgi-bin/operating-systems.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Content-type:text/html"
+echo
+
+. ./includes/nav.inc.sh "Accessible Operating Systems"
+cat <<END-OF-CAT
+<h2>Operating System Accessibility</h2>
+<p>Whether it's a linux distribution, the latest mac operating system or the hot new stuff from microsoft, or even google, we intend to cover how accessible it is to blind and visually impared users on this page to show you what choices you have when picking an OS as a blind computing community member.</p>
+END-OF-CAT
+. ./includes/resources.inc.sh operating-systems
+echo "</div></body></html>
+"
