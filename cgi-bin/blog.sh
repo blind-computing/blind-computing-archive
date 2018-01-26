c87181b029af3bf2cec291b479b87658653ae76d
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 10:01:55 +0000

    added devices and blog pages. The blog page is complete.

---
+++ cgi-bin/blog.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Content-type:text/html"
+echo
+
+. ./includes/nav.inc.sh "blog"
+cat <<END-OF-CAT
+<h2>The Blind Computing Blog</h2>
+<p>  This website has always been about uncovering up-to-date information on the general accessibility of technology, be it linux, mac os (wether it's legacy or current) and windows (from 1.01 to windows 10). It's main goal is to show people how to do things, or review things so that people know what they're bying. </p>
+<p>While this is all well and good, it poses the question, "Where should we get the information in the first place?" Because of this, I've decided to start a blind-computing blog, which will create a place where I and other contributers can pose questions to the community, provide updates on what we're working on,etc, without clogging up the main web site.</p>
+<p>  This new blog will be hosted by google using it's blogger service, which unfortunately means that while accessible, will be very slow and laborious to load and navigate with accessible software. To help get around this problem, I advise any person who wishes to read this to subscribe to the RSS/ATOM feed, which will greately improve the accessibility if a good feed reader is used.</p>
+<p><a href="http://bit.ly/2oTj1ZW">subscribe here</a> or <a href="https://blind-computing.blogspot.com">view the website</a></p>
+<hr>
+<script src="http://feeds.feedburner.com/Blind-computingBlog?format=sigpro" type="text/javascript" ></script><noscript><p>Subscribe to RSS headline updates from: <a href="http://feeds.feedburner.com/Blind-computingBlog"></a><br/>Powered by FeedBurner</p> </noscript>
+</div></body></html>
+END-OF-CAT
---
+++ cgi-bin/devices.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Content-type:text/html"
+echo
+
+. ./includes/nav.inc.sh "devices"
+cat <<END-OF-CAT
+<h2>Devices</h2>
+<p>  This page is dedicated to topics relating to various braille note takers and other devices that are useful to blind or visually impared people. Any contributions are welcome: these things are so expensive!</p>
+</div></body></html>
+END-OF-CAT
