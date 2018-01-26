2f41d6108d38b7df90da49968a9c0916b37e5c60
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 10:26:46 +0000

    added credit field in devices-items.csv to allow for author credit as well as writing the code to provide this info to the user.

--- cgi-bin/includes/resources.inc.sh
+++ cgi-bin/includes/resources.inc.sh
@@ -4,10 +4,11 @@ cat <<END-OF-CAT
 END-OF-CAT
 cat includes/${1}-items.csv | while read line
 do
-prefix="$(echo $line | cut -d, -f1 -)"
-lnktxt="$(echo $line | cut -d, -f2 -)"
-page="$(echo $line | cut -d, -f3 -)"
-printline="<li class=\"resource\">${prefix} <a href=\"${page}\">${lnktxt}</a>.</li>"
+credit="$(echo $line | cut -d, -f1 -)"
+prefix="$(echo $line | cut -d, -f2 -)"
+lnktxt="$(echo $line | cut -d, -f3 -)"
+page="$(echo $line | cut -d, -f4 -)"
+printline="<li class=\"resource\">${prefix} <a href=\"${page}\" title=\"Credit: ${credit}\">${lnktxt}</a>.</li>"
 echo "$printline"
 done
 cat <<END-OF-CAT
