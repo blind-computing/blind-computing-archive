0e2e999681bf2d735c398d5e75dcf46ab0202cd9
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 11:37:25 +0000

    Started work on adding message when no resources are available in resources.inc.sh, however it's not working yet

--- cgi-bin/includes/resources.inc.sh
+++ cgi-bin/includes/resources.inc.sh
@@ -2,14 +2,20 @@ cat <<END-OF-CAT
 <h3>List of Resources</h3>
 <ul class="resource-list">
 END-OF-CAT
-cat includes/${1}-items.csv | while read line
+filename="includes/${1}-items.csv"
+cat $filename | while read line
 do
-credit="$(echo $line | cut -d, -f1 -)"
-prefix="$(echo $line | cut -d, -f2 - | sed 's/~/,/g')"
-lnktxt="$(echo $line | cut -d, -f3 -	| sed 's/~/,/g')"
-page="$(echo $line | cut -d, -f4 -)"
-printline="<li class=\"resource\">${prefix} <a href=\"${page}\" title=\"Credit: ${credit}\">${lnktxt}</a>.</li>"
-echo "$printline"
+    if [ -r $filename ]
+    then
+        credit="$(echo $line | cut -d, -f1 -)"
+        prefix="$(echo $line | cut -d, -f2 - | sed 's/~/,/g')"
+        lnktxt="$(echo $line | cut -d, -f3 -	| sed 's/~/,/g')"
+        page="$(echo $line | cut -d, -f4 -)"
+        printline="<li class=\"resource\">${prefix} <a href=\"${page}\" title=\"Credit: ${credit}\">${lnktxt}</a>.</li>"
+        echo "$printline"
+    else
+        echo '<strong><p>No resources yet, check back later or <a href="https://github.com/mcb2003/blind-computing">submit a pull request to get your content here</a>.</p></strong>'
+    fi
 done
 cat <<END-OF-CAT
 </ul>
