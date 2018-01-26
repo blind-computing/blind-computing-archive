2e6fe3a59701240903fe15493357b4e0b46cce95
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 11:46:58 +0000

    completed support for non-existant resource csv files that display a message telling the user that there are no resources.

--- cgi-bin/includes/resources.inc.sh
+++ cgi-bin/includes/resources.inc.sh
@@ -3,19 +3,18 @@ cat <<END-OF-CAT
 <ul class="resource-list">
 END-OF-CAT
 filename="includes/${1}-items.csv"
+if ! [ -f "$filename" ]
+then
+    echo '<strong><p>No resources yet, check back later or <a href="https://github.com/mcb2003/blind-computing">submit a pull request to get your content here</a>.</p></strong>'
+fi
 cat $filename | while read line
 do
-    if [ -r $filename ]
-    then
-        credit="$(echo $line | cut -d, -f1 -)"
-        prefix="$(echo $line | cut -d, -f2 - | sed 's/~/,/g')"
-        lnktxt="$(echo $line | cut -d, -f3 -	| sed 's/~/,/g')"
-        page="$(echo $line | cut -d, -f4 -)"
-        printline="<li class=\"resource\">${prefix} <a href=\"${page}\" title=\"Credit: ${credit}\">${lnktxt}</a>.</li>"
-        echo "$printline"
-    else
-        echo '<strong><p>No resources yet, check back later or <a href="https://github.com/mcb2003/blind-computing">submit a pull request to get your content here</a>.</p></strong>'
-    fi
+    credit="$(echo $line | cut -d, -f1 -)"
+    prefix="$(echo $line | cut -d, -f2 - | sed 's/~/,/g')"
+    lnktxt="$(echo $line | cut -d, -f3 -	| sed 's/~/,/g')"
+    page="$(echo $line | cut -d, -f4 -)"
+    printline="<li class=\"resource\">${prefix} <a href=\"${page}\" title=\"Credit: ${credit}\">${lnktxt}</a>.</li>"
+    echo "$printline"
 done
 cat <<END-OF-CAT
 </ul>
