20deca08d24a9f5b23e419bcd1487ee65a59fe56
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 14:20:41 +0000

    Changes include: * Tweaked css a bit in regard to videos and their information * added the video.inc.sh include that automates embedding videos and their descriptions and publication dates. * Used this to recreate the old ubuntu-mate-16.04 video page.

---
+++ cgi-bin/includes/video.inc.sh
@@ -0,0 +1,8 @@
+echo "<iframe class=\"video-embed\" src=\"${1}\" frameborder=\"0\" allowfullscreen></iframe>"
+cat <<END-OF-CAT
+<div class="video-info">
+<h3>youtube video description</h3>
+END-OF-CAT
+echo "<p>Published on ${2}</p>"
+cat "includes/video-descriptions/${3}.html"
+echo "</div>"
---
+++ cgi-bin/ubuntu-mate-16.04-video.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Content-type:text/html"
+echo
+
+. ./includes/nav.inc.sh "Ubuntu Mate 16.04"
+cat <<END-OF-CAT
+<h2>ubuntu mate 16.04 accessibility</h2>
+<p>  Ubuntu mate is quickly growing to be a well respected, clean, easy to use linux distrobution.It has the latest features, the coolest software and the best desktop interface (in my opinion), but does it have the accessibility features that blind/vi people need out of an operating system?</p>
+<p>  Actually, the answer looks very promissing</p>
+<p>here's a quick video showing the accessibility features of ubuntu mate 16.04 lts.</p>
+END-OF-CAT
+. ./includes/video.inc.sh "https://www.youtube.com/embed/yCfBoCzOoEE" "12 Oct 2016" "ubuntu-mate-16.04"
+cat <<END-OF-CAT
+</div></body></html>
+END-OF-CAT
