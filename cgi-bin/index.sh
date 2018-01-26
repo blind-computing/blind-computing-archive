591be25efc56e7ba3b5b42715ada1965543e2bdb
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 12:04:05 +0000

    Added note to the bottom of the main page in index.html reccommending mozilla firefox to be used.

--- cgi-bin/index.sh
+++ cgi-bin/index.sh
@@ -9,5 +9,7 @@ cat <<END-OF-CAT
 <p>  Blind computing was actually started out of the demise of braillenoteusers.info, which is a site that the creater of this web site used a lot and found very helpfull. It aims to reconstruct the library allready existing on that website, however, we plan to cover a much broader scope of topics that are not related to the braille note.</p>
 <h2>how to contribute</h2>
 <p>The best way is to submit a pull request to the <a href="https://github.com/mcb2003/blind-computing" target="_blank" title="opens in a new tab">github repository</a>. These files are automatically uploaded to the site every three hours, so after the pull request has been accepted, you should weight a while to see the changes. That is, unless I'm nice and decide to force a re-upload.</p>
+<h2>Note</h2>
+<p>We reccommend you view this site in firefox if you're on a desktop, as this is what it's been tested in. Other browsers might work, but in general, mozilla's offeriing is more accessible anyway.</p>
 </div></body></html>
 END-OF-CAT
