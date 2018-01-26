6632a4cbb5b7a5d1a4c9960436bae49fbff41b86
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 15:32:15 +0000

    added webbioe article and changed some wording.

---
+++ cgi-bin/webbie-programs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Content-type:text/html"
+echo
+
+. ./includes/nav.inc.sh "The Webbie Programs"
+cat <<END-OF-CAT
+<h2>the webbie web browser and related tools</h2>
+<p>  These days, we all need a web browser, mainly to visit blind-computing,! Ok, really though, the internet's world wide web is becoming one of the biggest things in all of our lives (it's up there with chocolate and watching netflicks). So, how do blind computer users access the internet?</p>
+<p>  Of course, they could use one of the mainstream offerings for most tasks, such as google's chrome browser or mozilla's firefox, but these have their accessible limmits. For example, I personally find it very fidgity to use google chrome due to its lack of high contrast support and overall hard to navigate interface with a screen reader (probably due to webkit). That's where webbie comes in.</p>
+<h3>the webbie web browser</h3>
+<p>  The webbie web browser is a dot net application written in visual basic that uses the internet explorer frame work to render pages. It will strip out all images and graphical content, leaving the user with a list of items on the page. This layout is a sinch to use when you're scrolling down it with a screen reader (just press the up and down arrow keys), which makes it way easier than using chrome all ready. Clicking on a link does what you think it would do, and clicking on a form field will pop up a dialog box where that field can be filled out. Screen readers work extremely well with this way of navigation (it's just standard windows dialogs and fields).</p>
+<p>  You can also switch the browser into the "web" mode, which renders the pages normally, graphics and all, which will still provide about the same level of accessibility, but let's you see what you're doing (if you have any usable sight).</p>
+<p>  The browser also works well if you need high contrast theming or zoom, because as I stated earlier, the interface is made up of completely standard dot net/windows forms components.</p>
+<h3>The extras!</h3>
+<p>  Along with the web browser, the webbie suite of applications comes with a numbber of usefull tools including an accessible, text only PDF reader, a talking clock with reminders and alarms, a number of BBC Iplayer programs for listening to radio - be it live or pre-recorded - or watch BBC tv and an accessible file management program. You can download all of the programs in one bundle from the <a href="http://www.webbie.org.uk">webbie web site</a>, as well as downloading the programs separately</p>
+<h3>conclusion</h3>
+<p>  Overall, I love these programs. They're well designed, functional and useful. I particularly like the attention to detail, like the loading and finished loading sounds in the webbie web browser.</p>
+<p>  You can download the programs from the <a href="https://www.webbie.org/" title="visit the webbie website to download or get further information on the webbie wuite of applications">webbie web site</a>. While your there, checkout there blog - they've posted many articles on accessible software, such as a review of the <a href="https://www.webbie.org.uk/Veli-Pekka/ms_access_aids.html" title="go to the webbie blog article reviewing magnifier and narrator">narrator screen reader and the windows magnification tool</a>.</p>
+END-OF-CAT
