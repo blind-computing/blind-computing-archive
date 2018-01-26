579425725fd1dd263ee57ed791e3e344e5e49856
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-26 10:22:39 +0000

    added List of Resources heading in resources.inc.sh as well as adding full stops.

--- cgi-bin/devices.sh
+++ cgi-bin/devices.sh
@@ -6,5 +6,7 @@ echo
 cat <<END-OF-CAT
 <h2>Devices</h2>
 <p>  This page is dedicated to topics relating to various braille note takers and other devices that are useful to blind or visually impared people. Any contributions are welcome: these things are so expensive!</p>
-</div></body></html>
 END-OF-CAT
+. ./includes/resources.inc.sh devices
+echo "</div></body></html>
+"
--- cgi-bin/includes/resources.inc.sh
+++ cgi-bin/includes/resources.inc.sh
@@ -1,4 +1,5 @@
 cat <<END-OF-CAT
+<h3>List of Resources</h3>
 <ul class="resource-list">
 END-OF-CAT
 cat includes/${1}-items.csv | while read line
@@ -6,7 +7,7 @@ do
 prefix="$(echo $line | cut -d, -f1 -)"
 lnktxt="$(echo $line | cut -d, -f2 -)"
 page="$(echo $line | cut -d, -f3 -)"
-printline="<li class=\"resource\">${prefix} <a href=\"${page}\">${lnktxt}</a></li>"
+printline="<li class=\"resource\">${prefix} <a href=\"${page}\">${lnktxt}</a>.</li>"
 echo "$printline"
 done
 cat <<END-OF-CAT
