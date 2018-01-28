c74fc49075ef19271df650cf562f7ab9a419ddf4
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-01-28 18:54:55 +0000

    added what-is-linux.sh page

---
+++ cgi-bin/what-is-linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Content-type:text/html"
+echo
+
+. ./includes/nav.inc.sh "What Is Linux"
+cat <<END-OF-CAT
+<h1>What on Earth is Linux?</h1>
+<p>  So if you're new round here, you're probably asking, at almost every page you visit, "What in the actual world is linux?" Well, to explain what linux is, you need to know what an operating system is.</p>
+<h3>Operating Systems</h3>
+<p>  An operating system is essentially a piece of software, a program, that runs and manages the computer. In its simplest form, it displays stuff on the screen, allows you to smash that board of keys in fromt of you and is responsible for all those blue screens of death we all saw in the 90s. But seriously, an OS handles almost any task that you use a computer for in some way. Often programs can be loaded onto an operating system (they can be installed) to extend its functionality.</p>
+<p>There are many thousands of operating systems out there, the most common of which are microsoft's windows OS that has the largest marketshare and apple's mac OS, which comes on any apple computer you buy. Linux is one of these and I believe it's the future. Why? You may ask ...</p>
+<h3>The Concept of Opensource</h3>
+<p>  While typical operating systems that you can think of are made in house at some company, the linux OS is open to the public. So what does this mean? Essentially, anybody with the knowledge can <strong>contribute</strong> to a project and the community as a whole can form arround it, with each person scratching their particular itch and improving the OS as a whole. So in many less words, open source means that the code (source) is out in the open for anyone to contribute to.</p>
+<h3>The downsides</h3>
+<p>  Yes. As much as I don't want them to exist, this open model has its flaws. For example, if anyone can look at the code, anyone can find a security flaw. And while security researchers are constantly doing this to fix these holes, some people aren't like that and will take advantage of this opertunity. Now, I believe with enough community support that this can be overcome (in fact it allready has really), but it's a matter of opinion at this point.</p>
+<p>  And that's linux. I wrote this specifically to give blind and visually impared users a hint as to what I talk about alot on this site, so that they can make sense of it for themselves and choose which OS they want to use.</p>
+<p>Thanks for reading.</p>
+<i>Mikey</i>
+</div></body></html>
+END-OF-CAT
