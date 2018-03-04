d6555c21cf0ed4ac1a06171887285a561414e13d
root <root@blindcomputing.org>
2018-03-04 16:51:01 +0000

    Removed success output from db.connect.inc.php

--- includes/db.connect.inc.php
+++ includes/db.connect.inc.php
@@ -2,7 +2,5 @@
 $db = new mysqli("127.0.0.1","blindcomputing","Blind.Computing:21/01/2003","blindcomputing");
 if($db->connect_errno) {
     die("<h1>".$db->connect_error."</h1><h2>That's an error!</h2>");
-} else {
-    echo "<h1>success!</h1>";
 }
 ?>
