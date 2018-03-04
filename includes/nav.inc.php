56b49386742e847d80e96a55ee3652e7363834da
root <root@blindcomputing.org>
2018-03-04 17:46:07 +0000

    Made significant headway in the navigation
    * the main layout has been created.
    * The content has been styled to match the old site.
    * Mysql is now used to generate the main navigation.

---
+++ includes/nav.inc.php
@@ -0,0 +1,22 @@
+<?php
+require("db.connect.inc.php");
+?>
+<div id="headerbar">
+<a href="/" title="go  to the home page"><strong>/</strong></a>
+<h1 class="main-header">Blind Computing | <?php echo $TITLE; ?></h1>
+</div><div id="sidebar">
+<h2 class="small-header">Navigation</h2> <a href="#content">Skip to Content</a>
+<hr>
+<ul id="navigation">
+<?php
+if($result = $db->query("select title,description,uri from pages where category='main';")) {
+    while($row = $result->fetch_object()) {
+        echo '<li class="nav-item"><a href="', $row->uri, '" title="', $row->description, '">', $row->title, '</a></li>';
+    }
+} else {
+    die($db->error);
+}
+?>
+</ul>
+<hr>
+</div>
