a7df44def804d2b2ddc94720573806fe5dd338a3
root <root@blindcomputing.org>
2018-03-04 17:47:43 +0000

    Worked on home.php some more
    * A notice is shown regarding testing of this new version.
    * the header.inc.php, nav.inc.php and indirectly db.connect.inc.php files are included for max functionality.

--- home.php
+++ home.php
@@ -1,5 +1,15 @@
+<!doctype html>
+<html>
+<head>
 <?php
-require("includes/db.connect.inc.php");
-include("includes/nav.inc.php");
-echo "<h1>Welcome</h1><p>Hi. If you've made it here, you must be really good at working out that I'm testing things. Please feel free to look around, although it would be much easier to do so without errors or bugs by looking at the php-rewrite branch of the opensource code on github. Enjoy!</p>";
+$TITLE = "Home";
+include("includes/headers.inc.php");
 ?>
+</head><body>
+<?php
+include("includes/nav.inc.php");
+?>
+<div id="content">
+<h1>Welcome</h1><p>Hi. If you've made it here, you must be really good at working out that I'm testing things. Please feel free to look around, although it would be much easier to do so without errors or bugs by looking at the php-rewrite branch of the opensource code on github. Enjoy!</p>
+</div>
+</body></html>
