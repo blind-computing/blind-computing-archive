bd70c4c0e454d7aa9583f875f7cf964cbef850d0
root <root@blindcomputing.org>
2018-03-11 17:11:27 +0000

    Started work on operating-systems.php and blog.php

---
+++ blog.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<?php
+$TITLE = "Blog";
+include_once("includes/headers.inc.php");
+?>
+</head><body>
+<?php
+include("includes/nav.inc.php");
+?>
+<div id="content">
+<?php
+include_once("includes/functions.inc.php");
+echo create_category_page(
+        "The Blind Computing Blog",
+        "<p>  The main blind computing website has always been about giving blind and visually impaired computer users the most up to date information and tutorials we can, however, this poses the question, where do <strong>we</strong> get this information in the first place? That's what the Blind Computing Blog was created for. On it, we'll ask questions to the blind computing community, get answers via comments and talk about what we've been up to lately.</p><p>The blog is currently hosted on google's blogger service and you can visit using the links below. You can also subscribe to the RSS feed for a more accessible experience using your feed reader of choice.</p>",
+        "blog",
+        "");
+?>
+</div></body></html>
---
+++ operating-systems.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<?php
+$TITLE = "Operating Systems";
+include_once("includes/headers.inc.php");
+?>
+</head><body>
+<?php
+include("includes/nav.inc.php");
+?>
+<div id="content">
+<?php
+include_once("includes/functions.inc.php");
+echo create_category_page(
+        "Operating Systems",
+        "<p>An operating system is a big program that run's your device, whether  it be a braille note taker, desktop, laptop or even a phone. Operating systems control the overall user interface as well as the underlying technologies that make today's computing world possible.</p><p>There is an overwhelming amount of operating systems out there, from windows, to mac os 10 ... I mean mac OS because apple changed the name, to the thousands of linux distributions, right the way down to mobile operating systems like IOS and android. On this page, we review the accessibility features of these operating systems from a blind standpoint, showcasing there accessibility features (or lack there of).</p>",
+        "operating-systems");
+?>
+</div></body></html>
