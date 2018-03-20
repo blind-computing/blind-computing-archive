237cc47f9f295c83119374eda8b8cc98510c2853
root <root@blindcomputing.org>
2018-03-20 18:18:10 +0000

    updated everything, not sure what actually happened here.

---
+++ downloads.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<?php
+$TITLE = "Downloads";
+include_once("includes/headers.inc.php");
+?>
+</head><body>
+<?php
+include("includes/nav.inc.php");
+?>
+<div id="content">
+<?php
+include_once("includes/functions.inc.php");
+echo create_category_page(
+        "Downloads",
+        "<p>This page includes links to both externally and internally developed scripts, programs, operating systems and any other files you can think of to help the blind and visually impaired throughout their computing journey.</p><p><strong>Note: </strong> Some of the software on this page was <strong>not created by the contributers of Blind Computing</strong>. All credit goes to the creaters and maintainers of the software. Also, all of the files below are to be used at you're own risk and we can not assume any responsibility if something goes wrong.</p>",
+        "downloads",
+        "List of Downloads");
+?>
+</div></body></html>
