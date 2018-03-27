3887e5f4e7c8edf24c30a18b99fbb50783d9c939
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-03-27 16:29:30 +0000

    Replaced <div> containers with more screen reader friendly html5 elements.
    * Replaced the headerbar's <div> tags with <header> tags.
    * Replaced the sidebar's <div> tags with <aside> tags
    * Added <nav> tags arround the navigation list.

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,12 +1,13 @@
 <?php
 require_once("db.connect.inc.php");
 ?>
-<div id="headerbar">
+<header id="headerbar">
 <a href="/" title="go  to the home page" id="home-link"><strong>/</strong></a>
 <h1 class="main-header">Blind Computing | <?php echo $TITLE; ?></h1>
-</div><div id="sidebar">
+</header><aside id="sidebar">
 <h2 class="small-header">Navigation</h2> <a href="#content">Skip to Content</a>
 <hr>
+<nav>
 <ul id="navigation">
 <?php
 if($result = $db->query("select title,description,uri from pages where category='main';")) {
@@ -22,5 +23,6 @@ if($result = $db->query("select title,description,uri from pages where category=
 }
 ?>
 </ul>
+</nav>
 <hr>
-</div>
+</aside>
