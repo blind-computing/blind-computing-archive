86f1233a2e44176dacb3f1bb6b5b0746b18fc28f
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-03-28 15:47:12 +0000

    Moved "under construction" message to the actual home page and removed unneeded index.html. Also added back the usual contents of the home page

--- home.php
+++ home.php
@@ -10,6 +10,18 @@ include_once("includes/headers.inc.php");
 include("includes/nav.inc.php");
 ?>
 <main id="content"
-<h1>Welcome</h1><p>Hi. If you've made it here, you must be really good at working out that I'm testing things. Please feel free to look around, although it would be much easier to do so without errors or bugs by looking at the php-rewrite branch of the opensource code on github. Enjoy!</p>
+<center>
+<h2>We're Undergoing a Rewrite!</h2>
+<p>Currently, I'm working on completely redoing the site <strong>for the second time</strong> to make it much more robust and easier to manage. Much help is needed to get this done faster, so if you can, please <a href="https://github.com/mcb2003/blind-computing" title="opens the github page">help us rewrite the site in PHP with mysql</a>. If not, don't worry. The site will be finished soon and you can <a href="http://bit.ly/2oTj1ZW" title="opens the blind computing blog RSS feed">subscribe to the blog with your favourite rss news reader</a> to get alerted when the website's back. You can also check back at this page.</p>
+<p>Thanks for reading</p>
+<i>Mikey</i>
+<h2>What is Blind Computing?</h2>
+<p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributers have found useful.</p>
+<p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p>
+<h2>how to contribute</h2>
+<p>Please see the <a href="contributing.sh">contributing page</a> for more information.</p>
+<h2>Note</h2>
+<p>We recommend you view this site in mozilla firefox, found at <a href="https://firefox.com/download">the firefox download page</a>, if you're on a computer. Other browsers might work, but in general, firefox is more accessible anyway.</p>
+</section></body></html>
 </main>
 </body></html>
