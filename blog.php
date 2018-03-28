aad50599a1338abf37f8078f8e5f39254f27fcdf
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-03-28 19:28:38 +0000

    Added missing > sign after opening main tag in most php files.

--- blog.php
+++ blog.php
@@ -7,9 +7,9 @@ include_once("includes/headers.inc.php");
 ?>
 </head><body>
 <?php
-include("includes/nav.inc.php");
+include_once("includes/nav.inc.php");
 ?>
-<main id="content"
+<main id="content">
 <?php
 include_once("includes/functions.inc.php");
 echo create_category_page(
