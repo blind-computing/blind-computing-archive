1fecf551b3ef9e00e21872c1c0ece9b125b20d18
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-03-29 16:28:52 +0000

    Separated out different operating system resources by OS on operating-systems.php.
    *  Added hard coded "List of Resources" heading at level 2.
    * Added separate headings for windows, apple OSses and Linux, as well as separate resource lists.

--- operating-systems.php
+++ operating-systems.php
@@ -14,7 +14,14 @@ include("includes/nav.inc.php");
 include_once("includes/functions.inc.php");
 echo create_category_page(
         "Operating Systems",
-        "<p>An operating system is a big program that run's your device, whether  it be a braille note taker, desktop, laptop or even a phone. Operating systems control the overall user interface as well as the underlying technologies that make today's computing world possible.</p><p>There is an overwhelming amount of operating systems out there, from windows, to mac os 10 ... I mean mac OS because apple changed the name, to the thousands of linux distributions, right the way down to mobile operating systems like IOS and android. On this page, we review the accessibility features of these operating systems from a blind standpoint, showcasing there accessibility features (or lack there of).</p>",
-        "operating-systems");
+        "<p>An operating system is a big program that run's your device, whether  it be a braille note taker, desktop, laptop or even a phone. Operating systems control the overall user interface as well as the underlying technologies that make today's computing world possible.</p><p>There is an overwhelming amount of operating systems out there, from windows, to mac os 10 ... I mean mac OS because apple changed the name, to the thousands of linux distributions, right the way down to mobile operating systems like IOS and android. On this page, we review the accessibility features of these operating systems from a blind standpoint, showcasing there accessibility features (or lack there of).</p><h2>List of Resources</h2>",
+        "operating-systems-windows",
+        "<h3>Windows</h3>");
+echo create_resource_list(
+        "operating-systems-linux",
+        "<h3>Linux</h3>");
+echo create_resource_list(
+        "operating-systems-apple",
+        "<h3>Mac OS / IOS</h3>");
 ?>
 </main></body></html>
