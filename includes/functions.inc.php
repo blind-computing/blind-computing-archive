c2602832932f320e7b4c7451c2f7e6b95f3eb477
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-03-29 19:32:06 +0000

    Updated some stuff that i can't remember in functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -83,9 +83,9 @@ function create_video_page($title, $description, $id, $header = "Video Info") {
     $output = 
         '<h2>'.
         $title.
-        '</h2> <section id="description">'.
+        '</h2>\n<section id="description">'.
         $description.
-        '</section>';
+        '</section>\n';
     $output = $output.create_video_widget($id, $header);
     return $output;
 }
