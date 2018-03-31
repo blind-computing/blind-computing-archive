603e6ee0311777b6951dbc3fd03df13def43377c
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-03-31 16:18:17 +0000

    Added ubuntu mate 16.04 page.
      This page, like before, has the ubuntu mate 16.04 accessibility video on it, as well as the video's description.

---
+++ ubuntu-mate-16.04-video.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<?php
+$TITLE = "Ubuntu Mate 16.04";
+include_once("includes/headers.inc.php");
+?>
+</head><body>
+<?php
+include("includes/nav.inc.php");
+?>
+<main id="content">
+<?php
+require_once("includes/functions.inc.php");
+echo create_video_page(
+        "Accessibility of Ubuntu Mate 16.04 LTS",
+        "<p>  Ubuntu mate is quickly growing to be a well respected, clean, easy to use linux distrobution.It has the latest features, the coolest software and the best desktop interface (in my opinion), but does it have the accessibility features that blind/vi people need out of an operating system?</p><p>  Actually, the answer looks very promissing</p><p>here's a quick video showing the accessibility features of ubuntu mate 16.04 lts.</p>",
+        2);
+?>
+</main></body></html>
