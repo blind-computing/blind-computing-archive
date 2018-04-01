c9451e539f265be600496d16c69f012d0263f525
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-01 16:27:49 +0000

    Update port number in irc.php

--- irc.php
+++ irc.php
@@ -17,7 +17,7 @@ include("includes/nav.inc.php");
 <h3>IRC Channel Details</h3>
 <table>
 <tr><td><strong>Network: </strong></td><td>Freenode  <a href="irc://chat.freenode.net" title="To connect to this network, copy this link into your IRC client">chat.freenode.net</a></td></tr>
-<tr><td><strong>Port: </strong></td><td>6697</td></tr>
+<tr><td><strong>Port: </strong></td><td>6667</td></tr>
 <tr><td><strong>Channel: </strong></td><td>#blindcomputing</td></tr>
 </table>
 </section>
