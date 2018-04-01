f5f8014aa59713d5cd82503e1d1d73548a8843ed
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-01 13:37:22 +0000

    Updated all includes in video pages now located in videos/

--- video/ubuntu-17.10-video.php
+++ video/ubuntu-17.10-video.php
@@ -3,15 +3,15 @@
 <head>
 <?php
 $TITLE = "Ubuntu 17.10";
-include_once("includes/headers.inc.php");
+include_once("../includes/headers.inc.php");
 ?>
 </head><body>
 <?php
-include("includes/nav.inc.php");
+include("../includes/nav.inc.php");
 ?>
 <main id="content">
 <?php
-require_once("includes/functions.inc.php");
+require_once("../includes/functions.inc.php");
 echo create_video_page(
         "Accessibility of Ubuntu 17.10",
         "<p>It was the talk of the town when it came out! The new Canonical, releasing there latest version of ubuntu with ... the gnome desktop? Anyway, since this is a rather big change, we'll go over the accessibility features of this new release so that anyone upgrading to 17.10, or to 18.04 when it comes out, will have a smooth transition.</p>",
