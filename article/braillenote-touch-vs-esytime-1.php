131b5f3b34bbc5b89f24848a6806f93db3ace792
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-01 20:14:19 +0000

    Added first notetaker battle article.

---
+++ article/braillenote-touch-vs-esytime-1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+      $TITLE = "BrailleNote Touch VS EsyTime (1)?";
+      include_once("../includes/headers.inc.php");
+    ?>
+  </head>
+    
+  <body>
+    <?php
+      include("../includes/nav.inc.php");
+    ?>
+    <main id="content">
+      <?php
+        require_once("../includes/functions.inc.php");
+        echo create_article_info(4);
+      ?>
+      <article>
+<h2>Pros of the Braillenote Touch</h2>
+<ol>
+<li>It is possible to trade in an old Braillenote in order to make use of a discount.</li>
+<li>It runs android, which will tie in very well with the Google services at school.</li>
+<li>Humanware gives a good in depth manual and FAQ. You can find the FAQ at: http://www.humanware.com/microsite/bntouch/faq.php</li>
+<li>The touch has a newer version of bluetooth. This is really not a concern, but it is something to be aware of.</li>
+<li>The touch allows you to write touch braille which can be silent. It also gives you the option of using a perkins keyboard, included in the package.</li>
+<li>The touch has a 7in touch screen, while the esytime does not include a built in display of any kind except the braille display.</li>
+<li>Humanware is, at least in my mind, a very well respected company who I've had good results with in the past. Although the Apex crashes quite a bit, I've seldom if ever, been in a class and not been able to use my Braillenote. Recently, The Braillenote gave me a message that there was an error on the flash disk, which jerked me in to “panick and back up all data” mode, which meant that I didn't use it for that lesson, however, that has only happened once and I've used it ever since.</li>
+<li>The Braillenote Touch uses an SD card to store all data. Not only does this mean that, possibly, you could recover any data on this card in case of a failure, but the card is transfereable between Braillenote Touches. This can be usefull if the device breaks, as if a Humanware care plan is put in place, then they'll ship you a loner unit for you to insert the SD card into. Note, however, that this costs more.</li>
+<li>Humanware provides Youtube tutorials called Braillenote Touch snapshots that show you exactly how the operating system and some of the hardware works.</li>
+<li>The Braillenote Touch allows easy switching between 2 different language profiles, which changes the braille tables, as well as the speech system language.</li>
+</ol>
+<h2>Cons of the Braillenote Touch</h2>
+<ol>
+<li>The Braillenote Touch runs Android. While Android does integrate with Google services very well, all of the school's computers run Windows which means that the Esytime can technically run all of the same software, provided that the standard methods of implementing accessability have been deployed.</li>
+<li>I believe, although I haven't looked deeper than the FAQ at this time, that the Braillenote Touch will be more expensive than the Esytime, especially considering that I personally would much prefer a cell braille display (I feel as though a cell display would just be too small). When comparing this with the Esytime, which I believe has 32 cells as default, or even the Alva BC-640, which has 40 cells, it is something that would be great, but is certainly not needed.</li>
+<li>The braillenote touch has an hdmi port, which while it provides better video quality, all of the school's moniters use VGA like my old braillenote. This might not matter, because the touch has a built in display, however.</li>
+<li>The Braillenote Touch runs Android version 4.4, which is a few years old at this time. This isn't much of a problem, as Google is still providing security updates to 4.4 and it is estimated that 50%+ of Android devices run a software version less than 4.4 anyway, so app availability isn't much of a problem. Never-the-less, 7.0 is the latest.</li>
+</ol>
+<h2>Pros of the Esytime</h2>
+<ol>
+<li>The Esytime ships with Windows 7 pre-installed. Since all of the school computers run Windows this would mean that technically, I could run any piece of software they wished, even if it's not accessible, just with a screen and keyboard where ever I happen to be.</li>
+<li>Euro Braille is a fairly new company, which means that ideas are still fresh over there.</li>
+<li>Because the Esytime is just an embedded computer, even if the company drops support for the device, as long as somebody is making drivers for any newer operating system (like Windows 10 or possibly a Linux distribution) we can potentially install that operating system over windows 7 to get continued support from the OS manufacturer.</li>
+<li>The Esytime can be bought with some other options, such as an SSD instead of a hard drive (which we probably don't need), an external DVD-RW drive (I've already got one I can use), the Windows 7 Pro upgrade (this is what the school is running. I don't think it's necessary).</li>
+<li>The Esytime provides more storage space, because it uses a standard  laptop style hard disk drive. (#1)</li>
+<li>It is possible to install any screen reader of your choosing on the Esytime, as it is just a standard pc. It comes with NVDA as standard, so I can get up and running straight away.</li>
+<li>The Esytime can act as a braille display/terminal for another computer or an IOS device, such as an iPad. This is something I believe Humanware says that is not possible on the touch, or is not implemented yet (I believe it's the former).</li>
+<li>The Esytime, being Windows powered, will have better support for various devices, namely embossers and printers, when compared to the Android powered Braillenote Touch, which can print to any cloud print enabled printer.</li>
+<li>One option for the Esytime is a software package that is made for education. It offers a word processer that supports various braille languages, such as music braille. It also includes some extra programs, such as a calculater.</li>
+</ol>
+<h2>Cons of the Esytime</h2>
+<ol>
+<li>Because Euro Braille is a new company, they don't have any proven track record yet. I haven't sceen any reviews or pretty much any material about the Esytime on the internet, except the promotion.<li>
+<li>The Esytime runs Windows. When compared to Android, Windows needs virus protection, Malware bites and Spybot anti-spyware, and even then, it can slow down over time.</li>
+<li>I believe Windows 7's main stream support has ended, and I don't know what Microsoft are doing with extended support, as they are trying to push Windows 10 in everybody's faces.</li>
+</ol>
+<h3>Conclusion</h3>
+<p> First off, I want to say that when comparing these two note takers, I was able to find much more information about the Braillenote Touch. This means that the validity of this document is slightly floored, because I feel as though I'm giving the Esytime an unfair run for it's money. Anyway, I would also like to say that I've considered and will get a demo of each unit and will be posting a second report after the hands-on. I do express concerns over the fact that Euro braille is a new company, as with Optilec, well, let's just say that that experiment didn't work very well (that's why I want a demo), in contrast with Humannware, who have been making the Braille note family of products for years.  A fact that I can't seem to get over, that almost overwhelms the previous thought, is that the Esytime is an Open Hardware platform. What I mean by that is if you go to the promotion on their website, they list the name of the processer and how much ram and what disk they use; from that you can tell that all this machine is, is a PC in a small case with a permanently installed braille display and keyboard. This is a massive improvement over the Braillenote Touch. When the Touch is discontinued, it won't get security updates, but if the Esytime is discontinued I can just install something else, because it's just a PC.</p>
+<p> Okay, so the big question. Which one will I pick? Well… </p><p> I still don't know. I'll have to try both products and decide which one I like better in person. It's a very hard decision, because I love the fact that that the Esytime is open, but practically, I feel as though the touch will do better for me, at least at school. It's making me choose between open hardware, which is very important to me and practicallity, which obviously is crucial.</p>
+      </article>
+      </main>
+  </body>
+</html>
