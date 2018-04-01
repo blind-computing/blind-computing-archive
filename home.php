ea1aee6ca34f1fba5fc09ffd0c674c083ab01862
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-01 13:44:27 +0000

    Updated home.php so that the contributing link had an absolute path.

--- home.php
+++ home.php
@@ -22,7 +22,7 @@
         <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributers have found useful.</p>
         <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p>
         <h2>how to contribute</h2>
-        <p>Please see the <a href="contributing.php">contributing page</a> for more information.</p>
+        <p>Please see the <a href="/contributing.php">contributing page</a> for more information.</p>
         <h2>Note</h2>
         <p>We recommend you view this site in mozilla firefox, found at <a href="https://firefox.com/download">the firefox download page</a>, if you're on a computer. Other browsers might work, but in general, firefox is more accessible anyway.</p>
       </main>
