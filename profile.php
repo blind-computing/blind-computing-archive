24800a259d0f5421e97fdd758b3a275365ac8698
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-05 16:02:50 +0000

    Added basic profile.php template with nothing on it yet
      This page serves to generate profiles for every contributer to the site. It will be linked to, along with a get request for the username, by each page where a contributer's name is mentioned.

---
+++ profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php require_once("includes/db.connect.inc.php"); ?>
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <meta name="keywords" content="profile contributer author blindcomputing info information <?php echo $_GET['username']; ?>">
+  </head>
+    
+  <body>
+    <?php
+      include_once("includes/nav.inc.php");
+    ?>
+    <main id="content">
+
+    </main>
+  </body>
+</html>
