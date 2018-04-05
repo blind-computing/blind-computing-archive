042654d72790ca4f41b2b632837fc106c3303174
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-05 19:09:16 +0000

    Added <title> element to profile.php.

--- profile.php
+++ profile.php
@@ -7,6 +7,7 @@
       ?>
     <link rel="stylesheet" type="text/css" href="/css/style.css">
     <meta name="keywords" content="profile contributer author blindcomputing info information <?php echo $_GET['username']; ?>">
+    <title>BlindComputing | Profile: <?php echo $_GET['username']; ?></title>
   </head>
     
   <body>
