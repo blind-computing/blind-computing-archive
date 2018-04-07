91c854458caa041689535b06a6dd31e2c4af582b
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-07 13:30:16 +0000

    Remove the "We're undergoing a rewrite!" section from the home page.
      This notice was originally added back when the website was bearly even working and was in its infancy as a new rewrite. Now, the back end of the site is almost completely done, with little features to add and no major changes coming, so this message is not relevent any more. It has been removed.

--- home.php
+++ home.php
@@ -12,13 +12,6 @@
       include("includes/nav.inc.php");
     ?>
     <main id="content">
-      <center>
-        <h2>We're Undergoing a Rewrite!</h2>
-        <p>Currently, I'm working on completely redoing the site <strong>for the second time</strong> to make it much more robust and easier to manage. Much help is needed to get this done faster, so if you can, please <a href="https://github.com/mcb2003/blind-computing" title="opens the github page">help us rewrite the site in PHP with mysql</a>. If not, don't worry. The site will be finished soon and you can <a href="http://bit.ly/2oTj1ZW" title="opens the blind computing blog RSS feed">subscribe to the blog with your favourite rss news reader</a> to get alerted when it's ready. You can also check back at this page.</p>
-        <p>Thanks for reading</p>
-        <strong>Mikey</strong>
-      </center>
-        <hr>
         <h2>What is Blind Computing?</h2>
         <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributers have found useful.</p>
         <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p>
