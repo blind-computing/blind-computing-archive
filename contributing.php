275461fb31bc92a18b055686d6aaf73538e3375d
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-21 19:21:13 +0000

    Change link to github page in contributing.php.
      Since the blind-computing repository has moved to an organisation repository, it is good practice to change the URL that links to it on the contributing page.

--- contributing.php
+++ contributing.php
@@ -14,7 +14,7 @@ include("includes/nav.inc.php");
 <h3 id="byemail">By Email</h3>
 <p>If you would like to send any articles, videos or other information my way to be included into the website, you can <a href="mailto:michaelabuchan3370+blindcomputing@gmail.com">Email Me using this link</a>. This is the best option if you don't know how to use git and github, or for smaller pieces of information that need to be written up into articles or made into videos.</p>
 <h3 id="github">Using Github</h3>
-<p>Github is a great place to store code and the entire blind computing web site is up there. If you know what you're doing with git and github, you can <a href="https://github.com/mcb2003/blind-computing" target="about:blank">use this link to get to the github page</a>. If not, I suggest sending any content <a href="#byemail">through email</a>.</p>
+<p>Github is a great place to store code and the entire blind computing web site is up there. If you know what you're doing with git and github, you can <a href="https://github.com/blind-computing/blind-computing" target="about:blank">use this link to get to the github page</a>. If not, I suggest sending any content <a href="#byemail">through email</a>.</p>
 <strong>Thanks for all the support!</strong>
 </main>
 </body></html>
