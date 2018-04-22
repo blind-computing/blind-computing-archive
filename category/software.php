b013259f6c4ada6023e34f39d56b5c939c43d43f
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-04-22 10:31:43 +0000

    Update /category/software.php to correct gramma

--- category/software.php
+++ category/software.php
@@ -14,7 +14,7 @@ include("../includes/nav.inc.php");
 include_once("../includes/functions.inc.php");
 echo create_category_page(
         "Software",
-        '<p>It\'s all well and good having a good operating system, specifically chosen from <a href="operating-systems.php">our operating systems page</a>, but what use is it without any software to help get your work done? This page covers a wide range of software programs, tools and utilities to give you, the blind computer users, the most up to date information of program accessibility, or lack there of.</p>',
+        '<p>It\'s all well and good having a good operating system, specifically chosen from <a href="operating-systems.php">our operating systems page</a>, but what use is it without any software to help get your work done? This page covers a wide range of software programs, tools and utilities to give you, blind computer users, the most up to date information of program accessibility, or lack there of.</p>',
         "software");
 ?>
 </main></body></html>
