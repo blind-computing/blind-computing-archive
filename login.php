0fe0e605a569ecb82f7dd42b32406d7615f975ad
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-05-30 20:14:09 +0000

    *  Add login.php file, mostly a blank template right now. *  Modify robots.txt to disallow indexing of login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+      $id = 31;
+      include_once("includes/headers.inc.php");
+    ?>
+  </head>
+    
+  <body>
+    <?php
+      include("includes/nav.inc.php");
+    ?>
+    <main id="content">
+
+      </main>
+  </body>
+</html>
