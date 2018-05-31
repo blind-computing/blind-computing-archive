c9db4523ceb4646961956e406debf39af14c8423
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-05-31 19:54:48 +0000

    *  Fixed incorrect path specification in category/chat.php that meant tje mav.inc.php, headers.inc.php and functions.inc.php files couldn't be found.

--- category/chat.php
+++ category/chat.php
@@ -3,15 +3,15 @@
 <head>
 <?php
 $id = 12;
-include_once("includes/headers.inc.php");
+include_once("../includes/headers.inc.php");
 ?>
 </head><body>
 <?php
-include("includes/nav.inc.php");
+include_once("../includes/nav.inc.php");
 ?>
 <main id="content">
 <?php
-include_once("includes/functions.inc.php");
+require_once("../includes/functions.inc.php");
 echo create_category_page(
         "Chat and Contact",
         "<p>  <strong>This web site is a community!</strong>. With out the blind and visually impaired community, particularly the tech community, we wouldn't exist. So we've created various ways for each of you to chat and share code, ideas or discuss practically anything else.</p>",
