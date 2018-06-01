f01e4f0a97e999ea6b477e18a8c2218b59face35
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-06-01 21:50:31 +0000

    *  Removed unneeded escape character in category/software.php.

--- category/software.php
+++ category/software.php
@@ -12,7 +12,7 @@ include("../includes/nav.inc.php");
 <main id="content">
 <?php include_once("../includes/functions.inc.php"); ?>
 <h2>Accessible Software</h2>
-<p>It\'s all well and good having a good operating system, specifically chosen from <a href="operating-systems.php">our operating systems page</a>, but what use is it without any software to help get your work done? This page covers a wide range of software programs, tools and utilities to give you, blind computer users, the most up to date information of program accessibility, or lack there of.</p>
+<p>It's all well and good having a good operating system, specifically chosen from <a href="operating-systems.php">our operating systems page</a>, but what use is it without any software to help get your work done? This page covers a wide range of software programs, tools and utilities to give you, blind computer users, the most up to date information of program accessibility, or lack there of.</p>
 <h2>List of Resources</h2>
 <div class="third-width">
 <?php echo create_resource_list("software-showcase","<h3>Reviews and Overviews</h3>"); ?>
