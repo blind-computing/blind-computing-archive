cafd5f80411d4f63e8df52f3b78d1ab7c693cb49
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-06-01 21:37:55 +0000

    *  Add 3 column layout that is very similar to category/devices.php to category/software.php.

--- category/software.php
+++ category/software.php
@@ -10,11 +10,15 @@ include_once("../includes/headers.inc.php");
 include("../includes/nav.inc.php");
 ?>
 <main id="content">
-<?php
-include_once("../includes/functions.inc.php");
-echo create_category_page(
-        "Software",
-        '<p>It\'s all well and good having a good operating system, specifically chosen from <a href="operating-systems.php">our operating systems page</a>, but what use is it without any software to help get your work done? This page covers a wide range of software programs, tools and utilities to give you, blind computer users, the most up to date information of program accessibility, or lack there of.</p>',
-        "software");
-?>
-</main></body></html>
+<?php include_once("../includes/functions.inc.php"); ?>
+<h2>Accessible Software</h2>
+<p>It\'s all well and good having a good operating system, specifically chosen from <a href="operating-systems.php">our operating systems page</a>, but what use is it without any software to help get your work done? This page covers a wide range of software programs, tools and utilities to give you, blind computer users, the most up to date information of program accessibility, or lack there of.</p>
+<h2>List of Resources</h2>
+<div class="third-width">
+<?php echo create_resource_list("software-showcase","<h3>Reviews and Overviews</h3>"); ?>
+</div><div class="third-width">
+<?php echo create_resource_list("software-comparisons","<h3>Comparisons</h3>"); ?>
+</div><div class="third-width">
+<?php echo create_resource_list("software-news","Latest Software News</h3>"); ?>
+<p>Don't forget to stay up-to-date with the latest software news and announcements by <a href="https://www.twitter.com/blind_comp" target="blank">following us on twitter</a> and <a href="https://blind-computing.blogspot.com" target="blank">visiting the blog</a>.</p>
+</div></main></body></html>
