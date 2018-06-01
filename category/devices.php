20651c8787e5a50deb47bbabbd8b1ce4ac2898af
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-06-01 21:09:18 +0000

    *  Convert category/devices.php to 3 column layout, adding reviews, comparisons and latest news columns.

--- category/devices.php
+++ category/devices.php
@@ -10,11 +10,14 @@ include_once("../includes/headers.inc.php");
 include("../includes/nav.inc.php");
 ?>
 <main id="content">
-<?php
-include_once("../includes/functions.inc.php");
-echo create_category_page(
-        "Devices",
-        "<p>Whether it's braille notetakers, embossers, mainstream accessible gadgets or otherwise, this page covers a range of devices that can help out the blind and visually impaired. It includes thoughts, reviews, hand's on, rambling and much more!</p>",
-        "devices");
-?>
-</main></body></html>
+<?php include_once("../includes/functions.inc.php"); ?>
+<p>Whether it's braille notetakers, embossers, mainstream accessible gadgets or otherwise, this page covers a range of devices that can help out the blind and visually impaired. It includes thoughts, reviews, hand's on, rambling and much more!</p>
+<h2>List of Resources</h2>
+<div class="third-width">
+<?php echo create_resource_list("devices-reviews","<h3>Reviews</h3>"); ?>
+</div><div class="third-width">
+<?php echo create_resource_list("devices-comparisons","<h3>Comparisons</h3>"); ?>
+</div><div class="third-width">
+<?php echo create_resource_list("devices-news","<h3>Latest Device News</h3>"); ?>
+<p>Don't forget to stay up-to-date with the latest device news and announcements by <a href="https://www.twitter.com/blind_comp" target="blank">following us on twitter</a> and <a href="https://blind-computing.blogspot.com" target="blank">visiting the blog</a>.</p>
+</div></main></body></html>
