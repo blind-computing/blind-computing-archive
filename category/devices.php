0555ad9e4e3e59fb5d3fd5beb3145ff32a53322a
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-08-05 15:57:15 +0000

    Update all pages with third-width elements to include the new .padinner class on an inner div.

--- category/devices.php
+++ category/devices.php
@@ -14,10 +14,13 @@ include("../includes/nav.inc.php");
 <p>Whether it's braille notetakers, embossers, mainstream accessible gadgets or otherwise, this page covers a range of devices that can help out the blind and visually impaired. It includes thoughts, reviews, hand's on, rambling and much more!</p>
 <h2>List of Resources</h2>
 <div class="third-width">
+<div class="padinner">
 <?php echo create_resource_list("devices-reviews","<h3>Reviews</h3>"); ?>
-</div><div class="third-width">
+</div></div><div class="third-width">
+<div class="padinner">
 <?php echo create_resource_list("devices-comparisons","<h3>Comparisons</h3>"); ?>
-</div><div class="third-width">
+</div></div><div class="third-width">
+<div class="padinner">
 <?php echo create_resource_list("devices-news","<h3>Latest Device News</h3>"); ?>
 <p>Don't forget to stay up-to-date with the latest device news and announcements by <a href="https://www.twitter.com/blind_comp" target="blank">following us on twitter</a> and <a href="https://blind-computing.blogspot.com" target="blank">visiting the blog</a>.</p>
-</div></main></body></html>
+</div></div></main></body></html>
--- category/software.php
+++ category/software.php
@@ -15,10 +15,13 @@ include("../includes/nav.inc.php");
 <p>It's all well and good having a good operating system, specifically chosen from <a href="operating-systems.php">our operating systems page</a>, but what use is it without any software to help get your work done? This page covers a wide range of software programs, tools and utilities to give you, blind computer users, the most up to date information of program accessibility, or lack there of.</p>
 <h2>List of Resources</h2>
 <div class="third-width">
+<div class="padinner">
 <?php echo create_resource_list("software-showcase","<h3>Reviews and Overviews</h3>"); ?>
-</div><div class="third-width">
+</div></div><div class="third-width">
+<div class="padinner">
 <?php echo create_resource_list("software-comparisons","<h3>Comparisons</h3>"); ?>
-</div><div class="third-width">
+</div></div><div class="third-width">
+<div class="padinner">
 <?php echo create_resource_list("software-news","Latest Software News</h3>"); ?>
 <p>Don't forget to stay up-to-date with the latest software news and announcements by <a href="https://www.twitter.com/blind_comp" target="blank">following us on twitter</a> and <a href="https://blind-computing.blogspot.com" target="blank">visiting the blog</a>.</p>
-</div></main></body></html>
+</div></div></main></body></html>
