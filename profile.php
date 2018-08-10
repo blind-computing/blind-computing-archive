f558eb379f786c085f27e7939d27b84f317f96bf
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-08-10 00:23:52 +0000

    Completely redesign profile.php page. *  Removed old sidebar + description layout in favour of an informational header and description. *  The description is now in an <article> element for easier screen reader navigation. *  All informational content is within a <header> element for screen reader navigation. *  The table of social media and other links now takes up 100% width. *  Add custom header sizing for fullName and username headers, still preserving the correct heading levels. This is done through the .profile-header and .profile-subheader css classes.

--- profile.php
+++ profile.php
@@ -16,56 +16,54 @@
     ?>
     <main id="content">
       <?php
-        $contributorError = '<h1>404 - Contributer Not Found</h2><p>Sorry, the contributor you\'re looking for does not appear to exist. Make sure you:</p><ol><li>Provided a username to this page</li><li>Spelled the username correctly</li></ol>';
+        $contributor_error = '<h1>404 - Contributer Not Found</h1><p>Sorry, the contributor you\'re looking for does not appear to exist. Make sure you:</p><ol><li>Provided a username to this page</li><li>Spelled the username correctly</li></ol>';
+        $multiple_contributor_error = '<h1>Multiple contributors found: something is very very wrong!</h1><p>Please contact the blind computing team to report this error and give them the address of the page. Then just click the back button and forget about this.</p>';
         if(isset($_GET['username'])) {
           $username = $_GET['username'];
           // set up the database query and run it.
           $userResults = $db->prepare("select * from contributors where username=? limit 1;");
           $userResults->execute([$username]);
-          if($userResults->rowCount()) {
+          if($userResults->rowCount() == 1) {
             $user = $userResults->fetchObject();
-            echo '<aside class="contributor-info"><center><h2 class="contributor-header">'.$user->username.'</h2>';
+            echo '<header class="profile-info">';
             if($user->imguri != NULL) {
-              echo '<img aria-label="profile picture for '.$user->username.'." src="'.$user->imguri.'" class="profile-img-sidebar" width="128px" height="128px">';
+              echo '<img class="profile-img" aria-label="Profile picture for '.$user->username.'." src="'.$user->imguri.'">';
             }
-            echo '</center><hr><table>';
+            echo '<h2 class="profile-header">'.$user->username.'\'s Profile</h2>';
             if($user->fullName != NULL) {
-              echo '<tr><td><strong>Full Name: </strong></td><td>'.$user->fullName.'</td></tr>';
+              echo '<h3 class="profile-subheader">'.$user->fullName.'</h3>';
             }
-
+           echo '<table class="profile-information"><tbody>';
             if($user->email != NULL) {
-              echo '<tr><td><strong>Email: </strong></td><td><a target="blank" href="mailto:'.$user->email.'" title="Email '.$user->username.'">'.$user->email.'</a></td></tr>';
+              echo '<tr><th class="profile-th">Email: </th><td><a target="blank" href="mailto:'.$user->email.'" title="Email '.$user->username.'">'.$user->email.'</a></td></tr>';
             }
             if($user->github != NULL) {
               $githubUsername = substr(parse_url($user->github,PHP_URL_PATH),1);
-              echo '<tr><td><strong>GitHub: </strong></td><td><a href="'.$user->github.'" title="Go to the GitHub profile for '.$user->username.'">@'.$githubUsername.'</a></td></tr>';
+              echo '<tr><th class="profile-th">Github: </th><td><a href="'.$user->github.'" title="Go to the GitHub profile for '.$user->username.'">@'.$githubUsername.'</a></td></tr>';
             }
             if($user->twitter != NULL) {
               $twitterUsername = substr(parse_url($user->twitter,PHP_URL_PATH),1);
-              echo '<tr><td><strong>Twitter: </strong></td><td><a href="'.$user->twitter.'" title="Go to the Twitter Profile for '.$user->username.'">@'.$twitterUsername.'</a></td></tr>';
+              echo '<tr><th class="profile-th">Twitter: </th><td><a href="'.$user->twitter.'" title="Go to the Twitter Profile for '.$user->username.'">@'.$twitterUsername.'</a></td></tr>';
             }
             if($user->discordID != NULL && $user->discordName != NULL) {
-              echo '<tr><td><strong>Discord: </strong></td><td>@'.$user->discordName.'#'.$user->discordID.'</a></td></tr>';
+              echo '<tr><th class="profile-th">Discord: </th><td>@'.$user->discordName.'#'.$user->discordID.'</a></td></tr>';
             }
             if($user->youtube != NULL) {
-              echo '<tr><td><strong>YouTube: </strong></td><td><a href="'.$user->youtube.'" title="Go to '.$user->username.'\'s YouTube Channel">'.$user->username.'\'s Channel</a></td></tr>';
+              echo '<tr><th class="profile-th">YouTube: </th><td><a href="'.$user->youtube.'" title="Go to '.$user->username.'\'s YouTube Channel">'.$user->username.'\'s Channel</a></td></tr>';
             }
-            echo '</table><hr></aside>';
-          } else
-            echo $contributorError;
+            echo '</tbody></table>
+                 <hr>
+                 </header>
+                 <article class="profile-description">'.$user->description.'</article>';
+          } else if($userResults->rowCount() > 1) {
+            echo $multiple_contributor_error;
+          } else {
+            echo $contributor_error;
+          }
+        } else {
+          echo $contributor_error;
         }
       ?>
-      <section id="contributor-description">
-        <div class="padinner">
-        <?php
-          if($user->description != NULL) {
-            echo '<h3>Description</h3>';
-            echo $user->description;
-          } else
-            echo '<p>This contributor has not submitted a description yet.</p>';
-        ?>
-        </div>
-      </section>
     </main>
   </body>
 </html>
