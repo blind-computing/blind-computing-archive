af95d533f51d598421470ba93d34fc4d4a9535eb
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-08-12 14:26:11 +0000

    Change <h3> to <h2> in contributors.php for consistancy.

--- contributors.php
+++ contributors.php
@@ -26,7 +26,7 @@ include("includes/nav.inc.php");
   echo create_title("Our Contributors", "<p>  We wouldn't be nearly as far as we are with out them. Our contributors are the people that sat in front of a keyboard for sometimes hours on end to forge this site and make it what it is today. Of course, there are those who only contributed once, or for a short amount of time and they're just as important. So, we've grouped them all together into one page. Once again <strong>THANK YOU!</strong></p>
 ");
 ?>
-<h3>List of contributors</h3>
+<h2>List of contributors</h2>
 <?php
 echo create_contributor_list();
 ?>
