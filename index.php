953ace5f767c1aec5d622f4f13792a8a46d81da1
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-08-12 13:49:47 +0000

    Add <section> tags in homepage.

--- index.php
+++ index.php
@@ -14,19 +14,19 @@
     <main id="content">
         <div class="half-width">
           <div class="padinner">
-            <h2>What is Blind Computing?</h2>
+            <section><h2>What is Blind Computing?</h2>
             <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributors have found useful.</p>
-            <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p>
-            <h2>how to contribute</h2>
-            <p>Please see the <a href="/contributing.php">contributing page</a> for more information.</p>
-          <h2>Note</h2>
-          <p>We recommend you view this site in mozilla firefox, found at <a href="https://firefox.com/download">the firefox download page</a>, if you're on a computer. Other browsers might work, but in general, firefox is more accessible anyway.</p>
+            <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p></section>
+            <section><h2>how to contribute</h2>
+            <p>Please see the <a href="/contributing.php">contributing page</a> for more information.</p></section>
+          <section><h2>Note</h2>
+          <p>We recommend you view this site in mozilla firefox, found at <a href="https://firefox.com/download">the firefox download page</a>, if you're on a computer. Other browsers might work, but in general, firefox is more accessible anyway.</p></section>
         </div></div>
         <div class="halfwidth">
           <div class="padinner">
-            <iframe style="float:left; width: 44vw; height: 360px;" src="https://www.youtube.com/embed/tH9dlnxqtcU" frameborder="0" allow="encrypted-media" allowfullscreen="yes">Loading...</iframe>
-            <h2>Latest News</h2>
-            <p>Stay up to date with the latest news from Blind Computing by <a target="blank" href="http://blind-computing.blogspot.com">Visiting the blog</a>, <a target="blank" href="https://www.twitter.com/blind_comp">Following us on twitter</a> or <a target="blank" href="https://www.facebook.com/groups/347355422340125/">Joining our facebook group</a>.
+            <section><iframe style="float:left; width: 44vw; height: 360px;" src="https://www.youtube.com/embed/tH9dlnxqtcU" frameborder="0" allow="encrypted-media" allowfullscreen="yes">Loading...</iframe></section>
+            <section><h2>Latest News</h2>
+            <p>Stay up to date with the latest news from Blind Computing by <a target="blank" href="http://blind-computing.blogspot.com">Visiting the blog</a>, <a target="blank" href="https://www.twitter.com/blind_comp">Following us on twitter</a> or <a target="blank" href="https://www.facebook.com/groups/347355422340125/">Joining our facebook group</a>.</section>
         </div></div>
       </main>
   </body>
