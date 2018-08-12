e3b840f7618ebc6a5a9644404efb29025df0cfec
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-08-12 14:03:11 +0000

    converted the 'how to contribute' and 'note on browser support' sections into collapsible <details> elements.

--- index.php
+++ index.php
@@ -17,10 +17,15 @@
             <section><h2>What is Blind Computing?</h2>
             <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributors have found useful.</p>
             <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p></section>
-            <section><h2>how to contribute</h2>
-            <p>Please see the <a href="/contributing.php">contributing page</a> for more information.</p></section>
-          <section><h2>Note</h2>
-          <p>We recommend you view this site in mozilla firefox, found at <a href="https://firefox.com/download">the firefox download page</a>, if you're on a computer. Other browsers might work, but in general, firefox is more accessible anyway.</p></section>
+            <section><details>
+            <summary><h2>how to contribute</h2></summary>
+            <p>Please see the <a href="/contributing.php">contributing page</a> for more information.</p>
+          </details></section>
+          <section><details>
+            <summary><h2>Note on Browser Support</h2></summary>
+          <p>If you're on Linux or Windows, we recommend you view this site in mozilla firefox, found at <a href="https://firefox.com/download">the firefox download page</a>. Other browsers might work, but in general, firefox is more accessible anyway.</p>
+            <p> On a mac or iOS device, the built-in Safari browser seems to work fine as well. Chrome appears to be the second choice.</p>
+          </details></section>
         </div></div>
         <div class="halfwidth">
           <div class="padinner">
