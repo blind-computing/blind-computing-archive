1865edfe98969a2a61df21f4652df6198d832785
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-08-12 14:07:23 +0000

    Make 'What is Blind Computing?' section a <details> element. It is open by default.

--- index.php
+++ index.php
@@ -14,9 +14,11 @@
     <main id="content">
         <div class="half-width">
           <div class="padinner">
-            <section><h2>What is Blind Computing?</h2>
+            <section><details open>
+            <summary><h2>What is Blind Computing?</h2></summary>
             <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributors have found useful.</p>
-            <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p></section>
+            <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p>
+          </details></section>
             <section><details>
             <summary><h2>how to contribute</h2></summary>
             <p>Please see the <a href="/contributing.php">contributing page</a> for more information.</p>
