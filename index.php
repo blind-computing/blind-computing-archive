6b938e86f9d6848892ffc99af982d3d55c5a6852
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-09-18 16:09:43 +0000

    Add credit tooltip to logo in index.php.

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <main id="content">
         <div class="half-width">
           <div class="padinner">
-            <img aria-label="The Blind Computing Logo" class="hlogo" src="logo/horizontal.png" style="width:90%;">
+            <img aria-label="The Blind Computing Logo" title="Credit: @reallinfo on Github" class="hlogo" src="logo/horizontal.png" style="width:90%;">
             <section><details open>
             <summary><h2>What is Blind Computing?</h2></summary>
             <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributors have found useful.</p>
