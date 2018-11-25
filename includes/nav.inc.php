64f2ea6519305c430365d6074338743a5747707c
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-11-25 17:19:14 +0000

    Reworked nav.inc.php to use the new page class.

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,25 +1,28 @@
-<?php 	require_once("db.connect.inc.php"); ?>
+<?php
+    require_once 'db.connect.inc.php';
+    require_once 'page.class.php';
+?>
 
 <header id="headerbar">
-	<h1 class="main-header">Blind Computing | <?php echo $TITLE; ?></h1>
+<h1 class="main-header">
+Blind Computing<?php 
+if(isset($title)) {
+    echo " | " . $title;
+} ?>
+</h1>
 <hr class="headerstyle-hr">
 <nav><ul class="navbar">
-	<!-- <a href="#content"><li class="navItem">SkipToContent</li></a> -->
-    <?php
-        if($result = $db->query("select title,description,uri from pages where category='main';")) {
-          	while($row = $result->fetchObject()) {
-              	if($row->title === "-") {
-                	echo '<strong>  |  </strong>';
-              	} else {
-              		$isActivePage = "";
-              		if($TITLE == $row->title) { $isActivePage = "activePage";}
-                	echo '<a href="', $row->uri, '" title="', $row->description, '"><li class="navItem ', $isActivePage ,'">', $row->title ,'</li></a>';
-          		}
-          	}
-        } else {
-          	die($db->error);
+<?php
+    // get all the main pages.
+    $pages = page::get_pages_from_category("main");
+    if($pages != null) {
+        foreach($page as $pages) {
+            echo $page->get_navlink();
         }
-    ?>
+    } else {
+        echo "No nav items.";
+    }
+?>
 </ul></nav>
 <hr class="headerstyle-hr">
 </header>
