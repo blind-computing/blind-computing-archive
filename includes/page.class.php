2762649eb1ff78256666949957c98b6c6d3690ae
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-11-25 17:18:54 +0000

    Started work on page class.

---
+++ includes/page.class.php
@@ -0,0 +1,58 @@
+<?php
+    // include the database.
+    require_once 'db.connect.inc.php';
+    // this class handles the display of page links to any page on the site.
+    class page {
+        // property definitions.
+        public $id;
+        public $navTitle;
+        public $pageTitle;
+        public $navTooltip;
+        public $pageDescription;
+        public $uri;
+        public $tags;
+        public $category;
+        
+        // this function returns all page object from a specific category.
+        public static function get_pages_from_category(string $category) {
+            // make the db global.
+            global $db;
+            
+            // create the query.
+            $page_results = $db->prepare("SELECT * FROM pages WHERE category=?;");
+            if($query_successful = $page_results->execute([strtolower($category)]) && $page_results->rowCount()) {
+                // get the objects.
+                $pages = $page_results->fetchAll(PDO::FETCH_CLASS, "page");
+                return $pages;
+            } else {
+                return null;
+            }
+        }
+        
+        // this function returns a page object from a specific id.
+        public static function get_page_from_id(string $id) {
+            // make the db global.
+            global $db;
+            
+            // create the query.
+            $page_results = $db->prepare("SELECT * FROM pages WHERE id=?;");
+            if($query_successful = $page_results->execute([$id]) && $page_results->rowCount()) {
+                // get the object.
+                $page = $page_results->fetchObject("page");
+                return $page;
+            } else {
+                return null;
+            }
+        }
+        
+        // this function returns html for a navigation link for the main navbar.
+        public function get_navlink() {
+            $output = "<a href='{$this->uri}' title='{$this->pageDescription}'><li class='navItem ";
+            if(strtolower($title) == strtolower($this->navTitle)) {
+                $output .= "activePage";
+            }
+            $output .= "'>{$this->navTitle}</li></a>";
+            return $output;
+        }
+    }
+?>
