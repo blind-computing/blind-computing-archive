5c5e4db8fa2abf559b6928c2528553190e224735
mcb2003 <mikeybuchan@hotmail.co.uk>
2018-11-30 23:25:51 +0000

    Fixed a typeo in index.php.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <title>Massive Rework!</title>
 </head>
 <body>
-<h1>Massive Rewrork!</h1>
+<h1>Massive Rework!</h1>
 <p>The blind computing beta site will be down for quite a while while I rework a lot of the site to make it much more robust. Please stand by. In the mean time, you can <a href="https://blindcomputing.org">Visit the main site</a>.</p>
 <m>Mikey>
 </body>
