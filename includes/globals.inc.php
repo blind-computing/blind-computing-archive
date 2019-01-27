39457f38bdce315e48005bd9f08db41f46dab241
Michael Connor Buchan <mikey@mikeys-pc>
2019-01-27 16:58:54 +0000

    Started work on globals.inc.php file.

---
+++ includes/globals.inc.php
@@ -0,0 +1,11 @@
+<?php
+// This is the globals file
+    // It contains many global functions and variables needed to operate various parts of the site.
+    
+// This site class holds many of the important properties and methods, all of which are static, that are essential for various things.
+    class site {
+        public static $name = "Blind Computing";
+        public static $version = "4.0";
+        public static $domain = "https://blindcomputing.org";
+    }
+?>
