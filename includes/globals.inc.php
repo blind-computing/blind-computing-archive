af0edd923292f9efe2305e1bc97b0c30ff30077d
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-02-18 16:02:52 +0000

    Add page type mapping table in includes/globals.php in site class.

--- includes/globals.inc.php
+++ includes/globals.inc.php
@@ -7,6 +7,16 @@
         const name = "Blind Computing";
         const version = "4.0";
         const domain = "https://blindcomputing.org";
+        // this associative array holds the mappings between database page types and the strings used in URIs.
+        const page_type_mappings = array(
+            "main" => "",
+            "category" => "category/",
+            "article" => "article/",
+            "video" => "video/",
+            "software-showcase" => "software/",
+            "forum-thread" => "thread",
+            "project" => "project/"
+        );
 
         // this function converts a standard name string into a url safe string.
         public static function strtourl(string $str) {
