95c480e718c9e940f7e1569bcc9a230d373adf63
Michael Connor Buchan <mikey@mikeys-pc>
2019-02-20 21:49:17 +0000

    Added ids to headings and aria-labelledby attributes to sections in home.php.

--- home.php
+++ home.php
@@ -12,17 +12,17 @@
     <main id="content">
         <div class="half-width"margin-top: 0;">
           <div class="padinner" style="padding-top: 0;">
-            <section>
-            <h2>What is Blind Computing?</h2>
+            <section aria-labelledby="what-is-bc-heading">
+            <h2 id="what-is-bc-heading">What is Blind Computing?</h2>
             <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributors have found useful.</p>
             <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p>
           </section>
-            <section>
-            <h2>how to contribute</h2>
+            <section aria-labelledby="contributing-heading">
+            <h2 id="contributing-heading">how to contribute</h2>
             <p>Please see the <a href="/contributing.php">contributing page</a> for more information.</p>
           </section>
-          <section><details>
-            <summary><h2>Note on Browser Support</h2></summary>
+          <section aria-labelledby="browser-support-heading"><details>
+            <summary><h2 id="browser-support-heading">Note on Browser Support</h2></summary>
           <p>If you're on Linux or Windows, we recommend you view this site in mozilla firefox, found at <a href="https://firefox.com/download">the firefox download page</a>. Other browsers might work, but in general, firefox is more accessible anyway.</p>
           <p> On a mac or iOS device, the built-in Safari browser seems to work fine as well. <a href=https://www.google.com/chrome/"" title="Go to the Google Chrome Download page">Chrome</a> appears to be the second choice.</p>
           </details></section>
@@ -30,9 +30,9 @@
         <div class="halfwidth">
           <div class="padinner">
             <img aria-label="The Blind Computing Logo" title="Credit: @reallinfo on Github" class="hlogo" src="logo/horizontalv2.png" style="width:45%;">
-            <section><iframe style="float:left; width: 44vw; height: 360px;" src="https://www.youtube.com/embed/tH9dlnxqtcU" frameborder="0" allow="encrypted-media" allowfullscreen="yes">Loading...</iframe></section>
-            <section>
-            <h2>Latest News</h2>
+            <section aria-label="Video: An introduction to Blind Computing"><iframe style="float:left; width: 44vw; height: 360px;" src="https://www.youtube.com/embed/tH9dlnxqtcU" frameborder="0" allow="encrypted-media" allowfullscreen="yes">Loading...</iframe></section>
+            <section aria-labelledby="latest-news-heading">
+            <h2 id="latest-news-heading">Latest News</h2>
             <p>Stay up to date with the latest news from Blind Computing by <a target="blank" href="http://blind-computing.blogspot.com">Visiting the blog</a>, <a target="blank" href="https://www.twitter.com/blind_comp">Following us on twitter</a> or <a target="blank" href="https://www.facebook.com/groups/347355422340125/">Joining our facebook group</a>.
           </section>
         </div></div>
