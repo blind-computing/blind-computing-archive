71a06667c0c59928c36c34d517c411d9e1b5a127
Michael Connor Buchan <mikey@mikeys-pc>
2019-02-20 21:07:52 +0000

    Rename test.php to home.php and add back home content (will be renamed to index.php after the rewrite).

---
+++ home.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html><head>
+<?php
+    $title = "Home";
+?>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head><body>
+<?php
+    include_once 'includes/nav.inc.php';
+?>
+
+    <main id="content">
+        <div class="half-width">
+          <div class="padinner">
+            <img aria-label="The Blind Computing Logo" title="Credit: @reallinfo on Github" class="hlogo" src="logo/horizontal.png" style="width:90%;">
+            <section><details open>
+            <summary><h2>What is Blind Computing?</h2></summary>
+            <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributors have found useful.</p>
+            <p>  Blind computing was started out of the demise of braillenoteusers.info, a site that I used a lot and found very helpfull. It aims to reconstruct the library of braillenote content on that site, however, we plan to cover a much broader scope of topics that are not necessarily related to the braille note.</p>
+          </details></section>
+            <section><details>
+            <summary><h2>how to contribute</h2></summary>
+            <p>Please see the <a href="/contributing.php">contributing page</a> for more information.</p>
+          </details></section>
+          <section><details>
+            <summary><h2>Note on Browser Support</h2></summary>
+          <p>If you're on Linux or Windows, we recommend you view this site in mozilla firefox, found at <a href="https://firefox.com/download">the firefox download page</a>. Other browsers might work, but in general, firefox is more accessible anyway.</p>
+            <p> On a mac or iOS device, the built-in Safari browser seems to work fine as well. Chrome appears to be the second choice.</p>
+          </details></section>
+        </div></div>
+        <div class="halfwidth">
+          <div class="padinner">
+            <section><iframe style="float:left; width: 44vw; height: 360px;" src="https://www.youtube.com/embed/tH9dlnxqtcU" frameborder="0" allow="encrypted-media" allowfullscreen="yes">Loading...</iframe></section>
+            <section><details open>
+            <summary><h2>Latest News</h2></summary>
+            <p>Stay up to date with the latest news from Blind Computing by <a target="blank" href="http://blind-computing.blogspot.com">Visiting the blog</a>, <a target="blank" href="https://www.twitter.com/blind_comp">Following us on twitter</a> or <a target="blank" href="https://www.facebook.com/groups/347355422340125/">Joining our facebook group</a>.
+          </details></section>
+        </div></div>
+      </main>
+</body></html>
