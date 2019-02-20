417155d6290ddea30a3c63b3b77f40a4eaf084cc
Michael Connor Buchan <mikey@mikeys-pc>
2019-02-20 21:27:54 +0000

    Fix padding-top for left-hand column on home.php.

--- home.php
+++ home.php
@@ -10,8 +10,8 @@
 ?>
 
     <main id="content">
-        <div class="half-width">
-          <div class="padinner">
+        <div class="half-width"margin-top: 0;">
+          <div class="padinner" style="padding-top: 0;">
             <section>
             <h2>What is Blind Computing?</h2>
             <p>This website is the hub of content for blind and visually impaired users that use a computer. We will cover as much as we can, from blind/vi devices, to the latest operating systems, down to accessible tools, programs and web sites that contributors have found useful.</p>
