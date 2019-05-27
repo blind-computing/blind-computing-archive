7767d716c1195a2db578a605e053561c4c09c1b3
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-27 22:14:21 +0100

    Add font awesome stylesheet for icon support.

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -6,6 +6,8 @@
 
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-<!-- Scripts -->
+<!-- Font awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+    integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" < !--
+    Scripts -->
 <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
