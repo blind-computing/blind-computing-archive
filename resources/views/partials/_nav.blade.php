282d5cf5857bc68cdb5f6439f4fc4a5a68b7abbb
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-27 20:33:49 +0100

    Begin work on navbar.

---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,14 @@
+<header class="navcontainer">
+    <div class="navtop">
+
+    </div>
+    <ul class="nav navbottom" role="navigation">
+        <li class="nav-item"><a href="/" class="nav-link active">Home</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Devices</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Operating Systems</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Software</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Downloads</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Community</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
+    </ul>
+</header>
