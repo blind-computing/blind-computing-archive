2632808262ba7ee6e6126a04dd22dcc6de1f05f0
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-27 21:18:58 +0100

    Improve navbar styling and layout.

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,8 +1,14 @@
-<header class="navcontainer">
-    <div class="navtop">
-
-    </div>
-    <ul class="nav navbottom" role="navigation">
+<header class="navcontainer navbar navbar-expand-sm bg-light fixed-top">
+    <!-- logo -->
+    <img src="{{ Asset('images/logo/horizontal.png') }}" alt="Blind Computing" aria-label="Blind Computing Logo"
+        class="nav-logo navbar-brand">
+    <!-- nav toggler (for small screens) -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navliinks"
+        aria-label="Expand/collapse the navigation bar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    <!-- links -->
+    <ul class="navbar-nav nav" role="navigation" id="navlinks">
         <li class="nav-item"><a href="/" class="nav-link active">Home</a></li>
         <li class="nav-item"><a href="" class="nav-link">Devices</a></li>
         <li class="nav-item"><a href="" class="nav-link">Operating Systems</a></li>
@@ -11,4 +17,8 @@
         <li class="nav-item"><a href="" class="nav-link">Community</a></li>
         <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
     </ul>
+    <!-- Account menu -->
+    <div class="ml-auto">
+
+    </div>
 </header>
