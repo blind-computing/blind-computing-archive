9ba8353dc76440a569194253b011a2c594aa21c9
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-28 14:12:12 +0100

    Add default profile picture.

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'user_name', 'full_name', 'email', 'password', 'twitter', 'mastodon', 'discord', 'public_email', 'profile_picture', 'github', 'youtube' 'bio', 'type',
     ];
 
     /**
