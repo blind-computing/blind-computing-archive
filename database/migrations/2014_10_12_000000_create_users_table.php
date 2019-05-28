315a722433f912ff069c75eb0194a7d2b00ce6b2
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-28 14:17:23 +0100

    Add desiired fields to users table db migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('user_name')->unique();
+            $table->string('full_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('public_email')->default(false);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('profile_picture')->default(Asset('images/profile_default.png'));
+            $table->text('bio')->nullable();
+            $table->enum('type', ['user', 'contributer', 'admin', 'banned'])->default('user');
+            // Social accounts:
+            $table->string('twitter')->nullable();
+            $table->string('github')->nullable();
+            $table->string('mastodon')->nullable();
+            $table->string('youtube')->nullable();
+            $table->string('discord')->nullable();
         });
     }
 
