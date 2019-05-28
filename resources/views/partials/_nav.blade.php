041c998436608ae5c26a9ecd853d690255ffeffc
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-28 14:12:34 +0100

    Make right side of navbar have the navigation role.

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -18,7 +18,7 @@
         <li class="nav-item"><a href="" class="nav-link">Blog</a></li>
     </ul>
 
-    <ul class="nav navbar-nav ml-auto">
+    <ul class="nav navbar-nav ml-auto" role="navigation">
         @guest
         <li class="nav-item"><a href="{{ Route('login') }}" class="nav-link">Login</a></li>
         <li class="nav-item"><a href="{{ Route('register') }}" class="nav-link">Register</a></li>
