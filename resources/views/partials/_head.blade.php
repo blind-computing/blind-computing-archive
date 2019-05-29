1bdcf96b8a5b2f08a2cde57b2a9a5f8cfa60f395
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-29 15:30:44 +0100

    Fix malformed html in partials._head.

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -8,6 +8,6 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <!-- Font awesome -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-    integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" < !--
-    Scripts -->
+    integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
