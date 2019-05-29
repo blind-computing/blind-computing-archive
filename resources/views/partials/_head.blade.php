5b451168650c3f850702c35eeb3604810177dbfc
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-29 15:24:49 +0100

    Uncomment app.js file in partials._head. This is required for bootstrap dynamic features.

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -10,4 +10,4 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
     integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" < !--
     Scripts -->
-<!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+<script src="{{ asset('js/app.js') }}" defer></script>
