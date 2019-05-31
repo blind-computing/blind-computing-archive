65a83c358364de69de133a0f312e82ff0b06ece1
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-31 12:14:00 +0100

    Fix missing ; in Category model.

--- app/Category.php
+++ app/Category.php
@@ -11,6 +11,6 @@ class Category extends Model
      */
     public function parent()
     {
-        return $this->hasOne('App\Category', 'parent_id')
+        return $this->hasOne('App\Category', 'parent_id');
     }
 }
