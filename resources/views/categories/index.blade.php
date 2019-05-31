201521913e41d6cc1f45f0695d191e25f9d7bd5c
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-31 12:30:00 +0100

    Continue to design categories.index view. Assuming all goes well, categories will now be listed with buttons to edit and delete them.

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -8,7 +8,29 @@
         aria-label="New Category">+</a>
 </div>
 @if(count($categories))
-
+@foreach($categories as $category)
+<div class="row">
+    <div class="col-sm-9">
+        <h2>{{ $category->name }}</h2>
+        <small class="text-muted"><strong>Parent:</strong>
+            @if($category->parent_id != null)
+            <a href="#category{{ $category->parent->id }}">{{ $category->parent->name }}</a>
+            @else
+            None
+            @endif
+        </small>
+        <div class="category-description">
+            {!! $category->description !!}
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <a href="{{ Route('categories.edit', $category->id) }}" class="btn btn-default" role="button"
+            aria-label="edit"><i class="fas fa-edit"></i></a>
+        <a href="{{ Route('categories.delete', $category->id) }}" class="btn btn-default" role="button"
+            aria-label="delete"><i class="fas fa-trash"></i></a>
+    </div>
+</div>
+@endforeach
 @else
 <p>No categories were found.</p>
 @endif
