fdf712bd977c77adaa5929b12a3656e6dc26e452
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-31 11:55:49 +0100

    Create parent/child category relationship in Category model.

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Get the parent category (if any)
+     */
+    public function parent()
+    {
+        return $this->hasOne('App\Category', 'parent_id')
+    }
 }
