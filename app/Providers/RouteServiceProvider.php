128ed2ae286eec5ee55c87be3e1fc3956665c959
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-31 20:12:21 +0100

    Add more route patterns for user_name and full_name.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,8 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         Route::pattern('id', '[0-9]+');
+        Route::pattern('user_name', '^[a-zA-Z0-9\-_]+$');
+        Route::pattern('full_name', '^[a-zA-Z\- ]+$');
 
         parent::boot();
     }
