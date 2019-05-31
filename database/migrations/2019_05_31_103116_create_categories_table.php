510be29fd7987bff09832acae98b04f5b61fbd02
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-05-31 11:52:06 +0100

    Add required fields to categories migration.

--- database/migrations/2019_05_31_103116_create_categories_table.php
+++ database/migrations/2019_05_31_103116_create_categories_table.php
@@ -14,7 +14,10 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->unsigned();
+            $table->bigInteger('parent_id')->unsigned()->nullable();
+            $table->string('name');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
