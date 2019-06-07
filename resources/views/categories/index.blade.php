792a7d28160d208f277a897aa1a3320e931f64d5
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-06-07 19:35:12 +0100

    Begin componentisation of various elements, particularly the category header.

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -9,28 +9,11 @@
 </div>
 @if(count($categories))
 @foreach($categories as $category)
-<div class="row">
-    <div class="col-sm-9">
-        <h2><a href="{{ Route('categories.show', $category->id) }}">{{ $category->name }}</a></h2>
-        <small class="text-muted"><strong>Parent:</strong>
-            @if($category->parent_id != null)
-            <a href="{{ Route('categories.show', $category->parent->id) }}">{{ $category->parent->name }}</a>
-            @else
-            None
-            @endif
-        </small>
-        <div class="category-description">
-            {!! $category->description !!}
-        </div>
-    </div>
-    <form class="ml-auto mr-1" action="{{ Route('categories.destroy', $category->id) }}" method="post">
-        @csrf
-        <a href="{{ Route('categories.edit', $category->id) }}" class="btn btn-default" role="button"
-            aria-label="edit"><i class="fas fa-edit"></i></a>
-        <input type="hidden" name="_method" value="delete">
-        <button class="btn btn-danger" aria-label="delete"><i class="fas fa-trash"></i></button>
-    </form>
-</div>
+@component('components.category_header', [
+'category' => $category,
+'linked' => true
+])
+@endcomponent
 @endforeach
 @else
 <p>No categories were found.</p>
---
+++ resources/views/components/category_header.blade.php
@@ -0,0 +1,39 @@
+<div class="row">
+    <div class="col-sm-9">
+        @if($linked)
+        <a href="{{ Route('category', $category->name) }}">
+            <h2>
+                @else
+                <h1>
+                    @endif
+                    {{ $category->name }}
+                    @if($linked)
+            </h2>
+        </a>
+        @else
+        </h1>
+        @endif
+
+        @if(Auth::check() && Auth::user()->type == 'admin')
+        <small class="text-muted"><strong>Parent:</strong>
+            @if($category->parent_id != null)
+            {{ $category->parent->name }}
+            @else
+            None
+            @endif
+        </small>
+        @endif
+    </div>
+    @if(Auth::check() && Auth::user()->type == 'admin')
+    <form class="ml-auto mr-1" action="{{ Route('categories.destroy', $category->id) }}" method="post">
+        @csrf
+        <a href="{{ Route('categories.edit', $category->id) }}" class="btn btn-default" role="button"
+            aria-label="edit"><i class="fas fa-edit"></i></a>
+        <input type="hidden" name="_method" value="delete">
+        <button class="btn btn-danger" aria-label="delete"><i class="fas fa-trash"></i></button>
+    </form>
+    @endif
+</div>
+<div class="category-description">
+    {!! $category->description !!}
+</div>
