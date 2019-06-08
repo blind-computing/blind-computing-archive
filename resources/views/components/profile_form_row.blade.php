ec699cd0e80ea7832ff37e2dfd8d24f50aaa24d3
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-06-08 16:44:46 +0100

    Refined profile_for_row component for beter accessibility and options.

--- resources/views/components/profile_form_row.blade.php
+++ resources/views/components/profile_form_row.blade.php
@@ -1,13 +1,29 @@
+@php
+$type = isset($type) ? $type: "textt";
+$input_classes = "form-control";
+    $labels = $name . "-lbl1";
+if(!$prepend_at)
+{
+    $input_classes .= " col-md-6";
+    } else {
+    $labels .= " " . $name . "-lbl2";
+    }
+@endphp
+@error($name)
+@php
+$input_classes .= " is-invalid";
+@endphp
+@enderror
 <div class="form-group row">
-                    <label id="{{ $name }}-lbl1" class="form-label col-md-2">{{ $label }}:</label>
+                    <label id="{{ $name }}-lbl1" class="form-label col-md-2">{{ $label . ':' }}</label>
 @if($prepend_at)
                     <div class="input-group col-md-6">
                         <div class="input-group-prepend">
-                            <label id="twitter-lbl2" class="input-group-text">@</label>
+                            <label id="{{ $name . '-lbl2' }}" class="input-group-text">@</label>
                         </div>
                         @endif
-                        <input type="@isset($type) {{$type }} @else text @endisset" class="form-control @if(!$prepend_at) col-md-6 @endif @error($name) is-invalid @enderror" id="{{ $name }}" name="{{ $name }}" value="{{ $slot }}"
-                            aria-labelledby="{{ $name . '-lbl1' }} @if($prepend_at) {{ $name . '-lbl2' }} @endif" placeholder="{{ $placeholder }}"
+                        <input type="{{ $type }}" class="{{ $input_classes }}" id="{{ $name }}" name="{{ $name }}" value="{{ $slot }}"
+                            aria-labelledby="{{ $labels }}" placeholder="{{ $placeholder }}"
                             @isset($pattern)
                             pattern="{{ $pattern }}"
                             @endisset
