545b9aeda1fd8c934eed0e0e1052e9dd460891de
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-06-24 18:05:39 +0100

    Add preview custom property in Posts model.

--- app/Post.php
+++ app/Post.php
@@ -31,4 +31,22 @@ class Post extends Model
         return $this->belongsTo('App\Category');
     }
 
+    /**
+     *A shorter body used as a preview on category post listings.
+     @return string
+     */
+    public function preview()
+    {
+        // First, convert the body to plane text.
+        $body_Text = (new Html2Text\Html2Text($this->body))->getText();
+        // Next get the plane text body as an array of words.
+        $body_array = explode($body_text, ' ');
+        // If this is less than 20, just return the body (it's short enough).
+        if(count($body_array) <= 20)
+        {
+            return $this->body_text;
+        }
+        // If not (it's longer) take only the first 20 words.
+        return implode(array_slice($body_array, 0, 19), ' ') . ' ...';
+    }
 }
