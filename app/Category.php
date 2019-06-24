7846f7bdd633d5afa3fc5478af41ed032c3ec521
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-06-24 19:24:32 +0100

    Add top_posts function in Category model to retrieve top posts.

--- app/Category.php
+++ app/Category.php
@@ -30,4 +30,19 @@ class Category extends Model
         return $this->hasMany('App\Post');
     }
 
+    /**
+     * Get the top posts (displayed in subcategory columns on parent category pages).
+     */
+    public function top_posts()
+    {
+        /// Get all pinned posts
+        $pinned_posts = $this->posts()->where('pinned', true)->orderBy('created_at', 'asc')->get();
+        // Calculate how many more posts we need to fill 3 spaces.
+        $extra_posts = 3 - count($pinned_posts) < 0?
+            1: 3 - count($pinned_posts);
+        // Get this many normal posts, in reverse chronological order, from the database.
+        $recent_posts = $this->posts()->where('pinned', false)->orderBy('created_at', 'desc')->take($extra_posts)->get();
+        // Finally, merge the two datasets and return them.
+        return $pinned_posts->merge($recent_posts);
+    }
 }
