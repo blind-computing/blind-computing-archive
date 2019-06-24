bf10e6e2de1533bde172d279318d184b30935a15
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-06-24 16:21:49 +0100

    Completely implement posts.index view using post_header component.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,57 +1,24 @@
 @extends('layouts.app')
 
-@if (Auth::user() && Auth::user()->type == 'admin')
 @section('title', 'Admin | Posts')
-@else
-@section('title', 'Posts')
-@endif
 @section('content')
-<div class="row">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-8">
+<div class="row mb-2">
+    <div class="col-sm-9">
         <h1>Posts</h1>
     </div>
-    <div class="col-sm-2">
-        @if (Auth::user() && Auth::user()->type == 'admin')
+    <div class="ml-auto mr-2">
         <a href="{{ Route('posts.create') }}" class="btn btn-primary" role="button">New Post</a>
-        @endif
     </div>
 </div>
-@if (count($posts) >=1)
-<div class="row justify-content-center">
-    <div class="col-md-10">
-        @foreach ($posts as $post)
-        <div class="card bg-dark" role="article">
-            <header class="card-header post-header">
-                <div class="post-header-top">
-                    <h3 class="card-title post-title" aria-level="2">
-                        <a href="{{ Route('posts.show', $post->id) }}">{{ $post->title }}</a>
-                    </h3>
-                    @if ($post->pinned)
-                    <img aria-label='Pin Icon'
-                        src='http://icons.iconarchive.com/icons/paomedia/small-n-flat/32/pin-icon.png'>
-                    @endif
-                </div>
-                <small class="text-muted">
-                    Published on
-                    <time datetime="{{ $post->created_at }}">{{ $post->created_at }}</time>
-                    by <a href="/user/{{ $post->author_id }}">{{ $post->author->user_name }}</a>
-                </small>
-            </header>
-            <div class="card-body">
-                {!! $post->body !!}
-            </div>
-            @if (Auth::user() && Auth::user()->type == 'admin')
-            <div class="card-footer">
-                <form method="post" action="{{ Route('posts.destroy', $post->id) }}">
-                    @csrf
-                    <input type="hidden" name="_method" value="delete">
-                    <a href="{{ Route('posts.edit', $post->id) }}" class="btn btn-default" role="button">Edit</a>
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </div>
-            @endif
         </div>
+        @if (count($posts) >=1)
+        {{ $posts->links() }}
+        @foreach ($posts as $post)
+        @component('components.post_header', [
+    'post' => $post,
+    'linked' => true
+])
+@endcomponent
         @endforeach
         {{ $posts->links() }}
     </div>
