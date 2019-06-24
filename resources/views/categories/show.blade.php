38ea74597602a517d5789d55035272d3321d4196
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-06-24 19:47:03 +0100

    Separate subcategory columns into a category_column component.

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -16,22 +16,10 @@
 @if($category->parent_id == null && count($category->children))
 <div class="row">
 @foreach($category->children as $subcategory)
-<section class="col-sm">
-    <a href="{{ Route('category', $subcategory->name) }}" title="Click to see the entire category"><h2>{{ $subcategory->name }}</h2></a>
-@if($subcategory->posts->count())
-@foreach($subcategory->top_posts() as $post)
-@component('components.post_widget', [
-    'post' => $post
+@component('components.category_column', [
+    'category' => $subcategory
 ])
 @endcomponent
-@endforeach
-@if($subcategory->top_posts()->count() < $subcategory->posts->count())
-<a href="{{ Route('category', $subcategory->name) }}">See more</a>
-@endif
-@else
-    <p>No posts were found in this category.</p>
-@endif
-</section>
 @endforeach
 </div>
 @endif
