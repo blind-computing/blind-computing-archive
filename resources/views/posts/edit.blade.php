0dcee1a6517d892af8097dc0cf00418deff68f77
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-06-24 17:08:58 +0100

    Change method and action in posts.edit vew to be correct.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,8 +8,9 @@
 <h1>Edit Post</h1>
 </div>
             <div class="card-body">
-                <form method="POST" action="/posts">
+                <form method="POST" action="{{ Route('posts.update', $post->id) }}">
                     @csrf
+                    @method('put')
 
                     <div class="form-group row">
                         <label for="title" class="col-md-4 col-form-label text-md-right">Title</label>
