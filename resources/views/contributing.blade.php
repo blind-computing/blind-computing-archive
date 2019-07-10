47434c21a6dba376134561de8632e92c54d4dc53
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-07-10 17:37:51 +0000

    Start work on contributing page.

---
+++ resources/views/contributing.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'How to Contribute')
+@section('content')
+<h1>How to Contribute to Blind Computing</h1>
+<p>
+Thanks for taking interest in this project and thanks even more for wanting to help out with it!
+Below are just some of the ways you can contribute to Blind Computing.
+</p>
+<h2>Contributing Code</h2>
+<p>
+Blind Computing is, has been and always will be an <strong>open source</strong project.
+If you would like to help out, the code is available on <a href="https://github.com/blind-computing/blind-computing">Github</a>.
+I would specifically advise you to <a href="">Read the readme file</a in order to get a better idea of how this project is developed.
+</p>
+<p>
+If you want to contribute code, then fork, modify and submit a pull request for this repo and I, along with anyone else who is part of <a href="https://github.com/blind-computing">the blind-computing Github organisation</a> will check and hopefully approve your changes.
+</p>
+<h2>Submitting Bugs and Feature Requests</h2>
+<p>
+Although we've tried to squash as many bugs as possible and have implemented and/or are implementing as many useful features as we can, if you've found a glitch or have an idea for a site-wide (I.E. <strong>not a piece of content</strong>) suggestion, we'd love to hear it! You can propose these ideas or problems to us via <a href="https://github.com/blind-computing/blind-computing/issues">Github's issue tracker</a>.
+</p>
+<p>
+Be sure to check the previously submitted issues (both open and closed) to see if anyone else has already reported the issue or come up with a similar idea.
+</p>
+@endsection
