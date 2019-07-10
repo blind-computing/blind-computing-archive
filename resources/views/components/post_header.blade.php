2498c33f88fd9d345e821c5d26bf516b2e9b5fa7
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-07-10 15:48:38 +0000

    Use post_info component in post_header component.

--- resources/views/components/post_header.blade.php
+++ resources/views/components/post_header.blade.php
@@ -27,15 +27,10 @@
     @endif
 </div>
 @if($show_info)
-<div class="post-info">
-@if(auth::check() && auth::user()->type == 'admin')
-<strong>URL Slug:</strong> {{ $post->slug }}<br>
-@endif
-Published on <time datetime="{{ $post->created_at }}">{{ $post->created_at }}</time> by <a href="{{ Route('profile', $post->author->user_name) }}" title="View {{ $post->author->full_name . "'s" }} profile">{{ '(@' . $post->author->user_name . ')' }}</a> in category <a href="{{ Route('category', $post->category->slug) }}">{{ $post->category->name }}</a>
-@if($post->updated_at != null)
-<br>Edited on <time datetime="{{ $post->updated_at }}">{{ $post->updated_at }}</time>
-@endif
-</div>
+@component('components.post_info', [
+    'post' => $post
+])
+@endcomponent
 @endif
 </div>
 </div>
