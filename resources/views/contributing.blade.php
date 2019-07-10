79163ac643f01bbcbb513335a6b40acc09a9f85d
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-07-10 17:52:39 +0000

    Complete content for contributing page.

--- resources/views/contributing.blade.php
+++ resources/views/contributing.blade.php
@@ -9,9 +9,9 @@ Below are just some of the ways you can contribute to Blind Computing.
 </p>
 <h2>Contributing Code</h2>
 <p>
-Blind Computing is, has been and always will be an <strong>open source</strong project.
+Blind Computing is, has been and always will be an <strong>open source</strong> project.
 If you would like to help out, the code is available on <a href="https://github.com/blind-computing/blind-computing">Github</a>.
-I would specifically advise you to <a href="">Read the readme file</a in order to get a better idea of how this project is developed.
+I would specifically advise you to <a href="https://github.com/blind-computing/blind-computing/blob/master/README.md">read the readme file</a> in order to get a better idea of how this project is developed.
 </p>
 <p>
 If you want to contribute code, then fork, modify and submit a pull request for this repo and I, along with anyone else who is part of <a href="https://github.com/blind-computing">the blind-computing Github organisation</a> will check and hopefully approve your changes.
@@ -23,4 +23,11 @@ Although we've tried to squash as many bugs as possible and have implemented and
 <p>
 Be sure to check the previously submitted issues (both open and closed) to see if anyone else has already reported the issue or come up with a similar idea.
 </p>
+<h2>Submitting Content</h2>
+<p>
+The goal of Blind Computing is to centralise, clarify and make easily accessible all helpful content in the blind / visually impaired tech world.
+So, if you've got some content, either that you made yourself or you found helpful, we'd love to here from you.
+All you have to do is <a href="mailto:mikeybuchan@hotmail.co.uk">shoot me an email</a>, preferably with details on where the content you're sending came from and I'll see what I can do about publishing it here.
+</p>
+<p>Once again, <strong>THANK YOU FOR YOUR SUPPORT!</strong></p>
 @endsection
