8bd6bc1199dd0162e001c02ed4d38d456c14bf1f
mcb2003 <mikeybuchan@hotmail.co.uk>
2019-07-10 16:14:21 +0000

    Add ability to change yt_embed component height. This is done by passing a height variable to the component when including it in a view.

--- resources/views/components/yt_embed.blade.php
+++ resources/views/components/yt_embed.blade.php
@@ -1,4 +1,4 @@
-<iframe style="width: 100%; height: 50vh;" src="https://www.youtube.com/embed/{{ $video_id }}" frameborder="0"
+<iframe style="width: 100%; height: {{ isset($height)? $height: '50vh' }};" src="https://www.youtube.com/embed/{{ $video_id }}" frameborder="0"
     allow="encrypted-media" allowfullscreen="yes">
     Loading...
 </iframe>
